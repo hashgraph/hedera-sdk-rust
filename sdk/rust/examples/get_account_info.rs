763d7f38c8ca44cc5b691acfab4bc792f2ff1d2d
Ryan Leckey <ryan@launchbadge.com>
2022-05-29 17:01:58 -0700

    feat(rust): use clap/dotenv to improve example configuration
    Signed-off-by: Ryan Leckey <ryan@launchbadge.com>

--- sdk/rust/examples/get_account_info.rs
+++ sdk/rust/examples/get_account_info.rs
@@ -1,17 +1,26 @@
-use std::str::FromStr;
-
+use clap::Parser;
 use hedera::{AccountId, AccountInfoQuery, Client, PrivateKey};
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[clap(long, env)]
+    payer_account_id: AccountId,
+
+    #[clap(long, env)]
+    default_signer: PrivateKey,
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let _ = dotenv::dotenv();
+    let args = Args::parse();
+
     let client = Client::for_testnet();
 
-    client.set_payer_account_id(AccountId::from(6189));
-    client.add_default_signer(PrivateKey::from_str(
-        "7f7ac6c8025a15ff1e07ef57c7295601379a4e9a526560790ae85252393868f0",
-    )?);
+    client.set_payer_account_id(args.payer_account_id);
+    client.add_default_signer(args.default_signer);
 
-    let id = AccountId::from(1001);
+    let id = AccountId::from(34938045);
 
     let info = AccountInfoQuery::new()
         .account_id(id)
