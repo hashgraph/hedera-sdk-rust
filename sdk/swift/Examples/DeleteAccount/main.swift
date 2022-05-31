998359ca2d7c161658e8cfd9dd4e1fd51d97c195
Ryan Leckey <ryan@launchbadge.com>
2022-05-31 11:46:00 -0700

    chore(swift): remove unused newKey from delete account example

--- sdk/swift/Examples/DeleteAccount/main.swift
+++ sdk/swift/Examples/DeleteAccount/main.swift
@@ -9,8 +9,6 @@ struct Program {
         client.addDefaultSigner(
             PrivateKey("302c020100300506032b65700420adceb87b3667f6909ab77d4016055590fe0328346f8430c4d6e4871fa2fec409")!)
 
-        let newKey = PrivateKey.generateEd25519()
-
         let response = try await AccountDeleteTransaction()
             .transferAccountId(AccountId("6189")!)
             .deleteAccountId(AccountId("0.0.34952813")!)
