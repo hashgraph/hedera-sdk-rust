f34b1fb722c2211e2e63dad0e63bf5b86a37f170
Will Parks <will.parks@launchbadge.com>
2022-06-07 11:52:38 -0700

    feat(rust): implement `TokenGrantKycTransaction`
    Signed-off-by: Will Parks <will.parks@launchbadge.com>

--- sdk/rust/src/token/mod.rs
+++ sdk/rust/src/token/mod.rs
@@ -1,5 +1,7 @@
 mod token_id;
 mod token_associate_transaction;
+mod token_grant_kyc_transaction;
 
 pub use token_id::TokenId;
 pub use token_associate_transaction::{TokenAssociateTransaction, TokenAssociateTransactionData};
+pub use token_grant_kyc_transaction::{TokenGrantKycTransaction, TokenGrantKycTransactionData};
