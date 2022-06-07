e698928330be98e9065bb49763e7689d1430af0b
Will Parks <will.parks@launchbadge.com>
2022-06-07 12:03:12 -0700

    feat(rust): implement `TokenRevokeKycTransaction`
    Signed-off-by: Will Parks <will.parks@launchbadge.com>

--- sdk/rust/src/token/mod.rs
+++ sdk/rust/src/token/mod.rs
@@ -1,7 +1,9 @@
 mod token_id;
 mod token_associate_transaction;
 mod token_grant_kyc_transaction;
+mod token_revoke_kyc_transaction;
 
 pub use token_id::TokenId;
 pub use token_associate_transaction::{TokenAssociateTransaction, TokenAssociateTransactionData};
 pub use token_grant_kyc_transaction::{TokenGrantKycTransaction, TokenGrantKycTransactionData};
+pub use token_revoke_kyc_transaction::{TokenRevokeKycTransaction, TokenRevokeKycTransactionData};
