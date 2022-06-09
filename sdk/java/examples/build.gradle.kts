1700565777bb20b9f89a49647253154ae6a27450
Ryan Leckey <ryan@launchbadge.com>
2022-06-08 17:17:03 -0700

    chore(java): setup Java taskfile, thread example running through taskfiles

--- sdk/java/examples/build.gradle.kts
+++ sdk/java/examples/build.gradle.kts
@@ -15,3 +15,17 @@ dependencies {
     implementation(rootProject)
     implementation("com.fasterxml.jackson.core:jackson-databind:2.13.3")
 }
+
+tasks.addRule("Pattern: run<Example>: Runs an example.") {
+    val taskPattern = this
+
+    if (taskPattern.startsWith("run")) {
+        val taskName = taskPattern.removePrefix("run") + "Example"
+
+        task<JavaExec>(taskPattern) {
+            mainClass.set(taskName)
+            classpath = sourceSets["main"].runtimeClasspath
+            standardInput = System.`in`
+        }
+    }
+}
