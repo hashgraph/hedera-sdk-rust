574afaa8c531171dc188a0e31728a21c9656fd6e
Ryan Leckey <ryan@launchbadge.com>
2022-06-15 11:19:49 -0700

    refactor: remove Fraction type and use fraction crate

--- sdk/rust/src/token/custom_fees.rs
+++ sdk/rust/src/token/custom_fees.rs
@@ -1,14 +1,12 @@
-use std::fmt::{self, Debug, Display, Formatter};
-
+use fraction::Fraction;
 use hedera_proto::services;
 
-use crate::{AccountId, FromProtobuf, TokenId, ToProtobuf};
-use crate::fraction::Fraction;
+use crate::{AccountId, FromProtobuf, ToProtobuf, TokenId};
 
 /// A transfer fee to assess during a CryptoTransfer that transfers units of the token to which the
 /// fee is attached. A custom fee may be either fixed or fractional, and must specify a fee collector
 /// account to receive the assessed fees. Only positive fees may be assessed.
-#[derive(serde::Serialize, serde::Deserialize, Hash, PartialEq, Eq, Clone)]
+#[derive(serde::Serialize, serde::Deserialize, Debug, Hash, PartialEq, Eq, Clone)]
 #[repr(C)]
 pub struct CustomFee {
     /// The fee to be charged.
@@ -18,29 +16,15 @@ pub struct CustomFee {
     fee_collector_account_id: AccountId,
 }
 
-impl Debug for CustomFee {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "\"{}\"", self)
-    }
-}
-
-impl Display for CustomFee {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "fee_collector_account_id: {}, fee: {}", self.fee_collector_account_id, self.fee)
-    }
-}
-
 impl FromProtobuf for CustomFee {
     type Protobuf = services::CustomFee;
 
     fn from_protobuf(pb: Self::Protobuf) -> crate::Result<Self> {
         let fee = Fee::from_protobuf(pb_getf!(pb, fee)?)?;
-        let fee_collector_account_id = AccountId::from_protobuf(pb_getf!(pb, fee_collector_account_id)?)?;
+        let fee_collector_account_id =
+            AccountId::from_protobuf(pb_getf!(pb, fee_collector_account_id)?)?;
 
-        Ok(Self {
-            fee_collector_account_id,
-            fee
-        })
+        Ok(Self { fee_collector_account_id, fee })
     }
 }
 
@@ -56,7 +40,7 @@ impl ToProtobuf for CustomFee {
 }
 
 /// Represents the possible fee types.
-#[derive(serde::Serialize, serde::Deserialize, Hash, PartialEq, Eq, Clone)]
+#[derive(Debug, serde::Serialize, serde::Deserialize, Hash, PartialEq, Eq, Clone)]
 #[repr(C)]
 pub enum Fee {
     /// Fixed fee to be charged.
@@ -69,30 +53,20 @@ pub enum Fee {
     RoyaltyFee(RoyaltyFee),
 }
 
-impl Debug for Fee {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "\"{}\"", self)
-    }
-}
-
-impl Display for Fee {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::FixedFee(fixed_fee) => Display::fmt(fixed_fee, f),
-            Self::FractionalFee(fractional_fee) => Display::fmt(fractional_fee, f),
-            Self::RoyaltyFee(royalty_fee) => Display::fmt(royalty_fee, f),
-        }
-    }
-}
-
 impl FromProtobuf for Fee {
     type Protobuf = services::custom_fee::Fee;
 
     fn from_protobuf(pb: Self::Protobuf) -> crate::Result<Self> {
         match pb {
-            Self::Protobuf::FixedFee(fixed_fee) => Ok(Fee::FixedFee(FixedFee::from_protobuf(fixed_fee)?)),
-            Self::Protobuf::FractionalFee(fractional_fee) => Ok(Fee::FractionalFee(FractionalFee::from_protobuf(fractional_fee)?)),
-            Self::Protobuf::RoyaltyFee(royalty_fee) => Ok(Fee::RoyaltyFee(RoyaltyFee::from_protobuf(royalty_fee)?)),
+            Self::Protobuf::FixedFee(fixed_fee) => {
+                Ok(Fee::FixedFee(FixedFee::from_protobuf(fixed_fee)?))
+            }
+            Self::Protobuf::FractionalFee(fractional_fee) => {
+                Ok(Fee::FractionalFee(FractionalFee::from_protobuf(fractional_fee)?))
+            }
+            Self::Protobuf::RoyaltyFee(royalty_fee) => {
+                Ok(Fee::RoyaltyFee(RoyaltyFee::from_protobuf(royalty_fee)?))
+            }
         }
     }
 }
@@ -103,7 +77,9 @@ impl ToProtobuf for Fee {
     fn to_protobuf(&self) -> Self::Protobuf {
         match self {
             Self::FixedFee(fixed_fee) => Self::Protobuf::FixedFee(fixed_fee.to_protobuf()),
-            Self::FractionalFee(fractional_fee) => Self::Protobuf::FractionalFee(fractional_fee.to_protobuf()),
+            Self::FractionalFee(fractional_fee) => {
+                Self::Protobuf::FractionalFee(fractional_fee.to_protobuf())
+            }
             Self::RoyaltyFee(royalty_fee) => Self::Protobuf::RoyaltyFee(royalty_fee.to_protobuf()),
         }
     }
@@ -129,7 +105,7 @@ impl From<FractionalFee> for Fee {
 
 /// A fixed number of units (hbar or token) to assess as a fee during a CryptoTransfer that transfers
 /// units of the token to which this fixed fee is attached.
-#[derive(serde::Serialize, serde::Deserialize, Hash, PartialEq, Eq, Clone)]
+#[derive(Debug, serde::Serialize, serde::Deserialize, Hash, PartialEq, Eq, Clone)]
 #[repr(C)]
 pub struct FixedFee {
     /// The number of units to assess as a fee
@@ -138,18 +114,6 @@ pub struct FixedFee {
     /// The denomination of the fee; taken as hbar if left unset and, in a TokenCreate, taken as the id
     /// of the newly created token if set to the sentinel value of 0.0.0
     pub denominating_token_id: TokenId,
-}
-
-impl Debug for FixedFee {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "\"{}\"", self)
-    }
-}
-
-impl Display for FixedFee {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self)
-    }
 }
 
 impl FromProtobuf for FixedFee {
@@ -169,7 +133,7 @@ impl ToProtobuf for FixedFee {
     fn to_protobuf(&self) -> Self::Protobuf {
         Self::Protobuf {
             amount: self.amount,
-            denominating_token_id: Some(self.denominating_token_id.to_protobuf())
+            denominating_token_id: Some(self.denominating_token_id.to_protobuf()),
         }
     }
 }
@@ -177,7 +141,7 @@ impl ToProtobuf for FixedFee {
 /// A fraction of the transferred units of a token to assess as a fee. The amount assessed will never
 /// be less than the given minimum_amount, and never greater than the given maximum_amount.  The
 /// denomination is always units of the token to which this fractional fee is attached.
-#[derive(serde::Serialize, serde::Deserialize, Hash, PartialEq, Eq, Clone)]
+#[derive(Debug, serde::Serialize, serde::Deserialize, Hash, PartialEq, Eq, Clone)]
 #[repr(C)]
 pub struct FractionalFee {
     /// The fraction of the transferred units to assess as a fee
@@ -195,18 +159,6 @@ pub struct FractionalFee {
     pub net_of_transfers: bool,
 }
 
-impl Debug for FractionalFee {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "\"{}\"", self)
-    }
-}
-
-impl Display for FractionalFee {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self)
-    }
-}
-
 impl FromProtobuf for FractionalFee {
     type Protobuf = services::FractionalFee;
 
@@ -215,7 +167,7 @@ impl FromProtobuf for FractionalFee {
             net_of_transfers: pb.net_of_transfers,
             maximum_amount: pb.maximum_amount,
             minimum_amount: pb.minimum_amount,
-            fractional_amount: Fraction::from_protobuf(pb_getf!(pb, fractional_amount)?)?,
+            fractional_amount: pb.fractional_amount.map(Into::into).unwrap_or_default(),
         })
     }
 }
@@ -225,7 +177,7 @@ impl ToProtobuf for FractionalFee {
 
     fn to_protobuf(&self) -> Self::Protobuf {
         Self::Protobuf {
-            fractional_amount: Some(self.fractional_amount.to_protobuf()),
+            fractional_amount: Some(self.fractional_amount.into()),
             minimum_amount: self.minimum_amount,
             maximum_amount: self.maximum_amount,
             net_of_transfers: self.net_of_transfers,
@@ -238,7 +190,7 @@ impl ToProtobuf for FractionalFee {
 /// value" includes both ℏ and units of fungible HTS tokens.) When the NFT sender does not receive
 /// any fungible value, the ledger will assess the fallback fee, if present, to the new NFT owner.
 /// Royalty fees can only be added to tokens of type type NON_FUNGIBLE_UNIQUE.
-#[derive(serde::Serialize, serde::Deserialize, Hash, PartialEq, Eq, Clone)]
+#[derive(Debug, serde::Serialize, serde::Deserialize, Hash, PartialEq, Eq, Clone)]
 #[repr(C)]
 pub struct RoyaltyFee {
     /// The fraction of fungible value exchanged for an NFT to collect as royalty
@@ -249,25 +201,13 @@ pub struct RoyaltyFee {
     pub fallback_fee: FixedFee,
 }
 
-impl Debug for RoyaltyFee {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "\"{}\"", self)
-    }
-}
-
-impl Display for RoyaltyFee {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self)
-    }
-}
-
 impl FromProtobuf for RoyaltyFee {
     type Protobuf = services::RoyaltyFee;
 
     fn from_protobuf(pb: Self::Protobuf) -> crate::Result<Self> {
         Ok(Self {
             fallback_fee: FixedFee::from_protobuf(pb_getf!(pb, fallback_fee)?)?,
-            exchange_value_fraction: Fraction::from_protobuf(pb_getf!(pb, exchange_value_fraction)?)?,
+            exchange_value_fraction: pb.exchange_value_fraction.map(Into::into).unwrap_or_default(),
         })
     }
 }
@@ -278,7 +218,7 @@ impl ToProtobuf for RoyaltyFee {
     fn to_protobuf(&self) -> Self::Protobuf {
         Self::Protobuf {
             fallback_fee: Some(self.fallback_fee.to_protobuf()),
-            exchange_value_fraction: Some(self.exchange_value_fraction.to_protobuf()),
+            exchange_value_fraction: Some(self.exchange_value_fraction.into()),
         }
     }
 }
@@ -286,26 +226,25 @@ impl ToProtobuf for RoyaltyFee {
 #[cfg(test)]
 mod tests {
     use hedera_proto::services;
-    use crate::{AccountId, FromProtobuf, TokenId, ToProtobuf};
+
     use crate::fraction::Fraction;
     use crate::token::custom_fees::{CustomFee, Fee, FixedFee, FractionalFee, RoyaltyFee};
+    use crate::{AccountId, FromProtobuf, ToProtobuf, TokenId};
 
     #[test]
     fn custom_fee_can_convert_to_protobuf() -> anyhow::Result<()> {
         let custom_fee = CustomFee {
             fee_collector_account_id: AccountId::from(1),
-            fee: Fee::FixedFee(
-                FixedFee {
-                    denominating_token_id: TokenId::from(2),
-                    amount: 1000
-                }
-            )
+            fee: Fee::FixedFee(FixedFee { denominating_token_id: TokenId::from(2), amount: 1000 }),
         };
 
         let custom_fee_proto = custom_fee.to_protobuf();
 
         assert_eq!(Some(custom_fee.fee.to_protobuf()), custom_fee_proto.fee);
-        assert_eq!(Some(custom_fee.fee_collector_account_id.to_protobuf()), custom_fee_proto.fee_collector_account_id);
+        assert_eq!(
+            Some(custom_fee.fee_collector_account_id.to_protobuf()),
+            custom_fee_proto.fee_collector_account_id
+        );
 
         Ok(())
     }
@@ -315,15 +254,18 @@ mod tests {
         let custom_fee_proto = services::CustomFee {
             fee: Some(services::custom_fee::Fee::FixedFee(services::FixedFee {
                 denominating_token_id: Some(TokenId::from(2).to_protobuf()),
-                amount: 1000
+                amount: 1000,
             })),
-            fee_collector_account_id: Some(AccountId::from(1).to_protobuf())
+            fee_collector_account_id: Some(AccountId::from(1).to_protobuf()),
         };
 
         let custom_fee = CustomFee::from_protobuf(custom_fee_proto.clone()).unwrap();
 
         assert_eq!(Some(custom_fee.fee.to_protobuf()), custom_fee_proto.fee);
-        assert_eq!(Some(custom_fee.fee_collector_account_id.to_protobuf()), custom_fee_proto.fee_collector_account_id);
+        assert_eq!(
+            Some(custom_fee.fee_collector_account_id.to_protobuf()),
+            custom_fee_proto.fee_collector_account_id
+        );
 
         Ok(())
     }
@@ -331,16 +273,13 @@ mod tests {
     #[test]
     fn fee_can_convert_to_protobuf() -> anyhow::Result<()> {
         let amount = 1000;
-        let fee = Fee::FixedFee(FixedFee {
-            amount,
-            denominating_token_id: TokenId::from(1),
-        });
+        let fee = Fee::FixedFee(FixedFee { amount, denominating_token_id: TokenId::from(1) });
 
         let fee_proto = fee.to_protobuf();
 
         let fixed_fee_proto = match fee_proto {
             services::custom_fee::Fee::FixedFee(fixed_fee) => Some(fixed_fee),
-            _ => None
+            _ => None,
         };
 
         assert_eq!(fixed_fee_proto.unwrap().amount, amount);
@@ -353,14 +292,14 @@ mod tests {
         let amount = 1000;
         let fee_proto = services::custom_fee::Fee::FixedFee(services::FixedFee {
             denominating_token_id: Some(TokenId::from(2).to_protobuf()),
-            amount
+            amount,
         });
 
         let custom_fee = Fee::from_protobuf(fee_proto).unwrap();
 
         let fixed_fee = match custom_fee {
             Fee::FixedFee(fixed_fee) => Some(fixed_fee),
-            _ => None
+            _ => None,
         };
 
         assert_eq!(fixed_fee.unwrap().amount, amount);
@@ -371,10 +310,7 @@ mod tests {
     #[test]
     fn fixed_fee_can_convert_to_protobuf() -> anyhow::Result<()> {
         let amount = 1000;
-        let fixed_fee = FixedFee {
-            amount,
-            denominating_token_id: TokenId::from(2),
-        };
+        let fixed_fee = FixedFee { amount, denominating_token_id: TokenId::from(2) };
 
         let fixed_fee_proto = fixed_fee.to_protobuf();
 
@@ -388,7 +324,7 @@ mod tests {
         let amount = 1000;
         let fixed_fee_proto = services::FixedFee {
             amount,
-            denominating_token_id: Some(TokenId::from(2).to_protobuf())
+            denominating_token_id: Some(TokenId::from(2).to_protobuf()),
         };
 
         let fixed_fee = FixedFee::from_protobuf(fixed_fee_proto).unwrap();
@@ -408,7 +344,7 @@ mod tests {
             fractional_amount: Fraction { numerator: 1, denominator: 2 },
             minimum_amount,
             maximum_amount,
-            net_of_transfers
+            net_of_transfers,
         };
 
         let fractional_fee_proto = fractional_fee.to_protobuf();
@@ -430,7 +366,7 @@ mod tests {
             fractional_amount: Some(services::Fraction { numerator: 1, denominator: 2 }),
             minimum_amount,
             maximum_amount,
-            net_of_transfers
+            net_of_transfers,
         };
 
         let fractional_fee = FractionalFee::from_protobuf(fractional_fee_protobuf).unwrap();
@@ -447,15 +383,16 @@ mod tests {
         let fallback_fee = FixedFee { denominating_token_id: TokenId::from(1), amount: 1000 };
         let exchange_value_fraction = Fraction { numerator: 1, denominator: 2 };
 
-        let royalty_fee = RoyaltyFee {
-            fallback_fee: fallback_fee.clone(),
-            exchange_value_fraction,
-        };
+        let royalty_fee =
+            RoyaltyFee { fallback_fee: fallback_fee.clone(), exchange_value_fraction };
 
         let royalty_fee_proto = royalty_fee.to_protobuf();
 
         assert_eq!(royalty_fee_proto.fallback_fee, Some(fallback_fee.to_protobuf()));
-        assert_eq!(royalty_fee_proto.exchange_value_fraction, Some(exchange_value_fraction.to_protobuf()));
+        assert_eq!(
+            royalty_fee_proto.exchange_value_fraction,
+            Some(exchange_value_fraction.to_protobuf())
+        );
 
         Ok(())
     }
@@ -466,12 +403,15 @@ mod tests {
         let numerator = 1;
         let denominator = 2;
 
-        let fallback_fee = services::FixedFee { denominating_token_id: Some(TokenId::from(1).to_protobuf()), amount };
+        let fallback_fee = services::FixedFee {
+            denominating_token_id: Some(TokenId::from(1).to_protobuf()),
+            amount,
+        };
         let exchange_value_fraction = services::Fraction { numerator, denominator };
 
         let royalty_fee_proto = services::RoyaltyFee {
             fallback_fee: Some(fallback_fee),
-            exchange_value_fraction: Some(exchange_value_fraction)
+            exchange_value_fraction: Some(exchange_value_fraction),
         };
 
         let royalty_fee = RoyaltyFee::from_protobuf(royalty_fee_proto).unwrap();
