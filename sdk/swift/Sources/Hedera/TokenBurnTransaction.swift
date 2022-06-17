34dea810f2c2fa1e0276f844a4e8c4390b86a634
Ryan Leckey <ryan@launchbadge.com>
2022-06-16 17:15:12 -0700

    feat(swift): add TokenBurnTransaction, TokenFreezeTransaction, TokenUnfreezeTransaction, TokenPauseTransaction, and TokenUnpauseTransaction

---
+++ sdk/swift/Sources/Hedera/TokenBurnTransaction.swift
@@ -0,0 +1,62 @@
+/// Burns tokens from the token's treasury account.
+public class TokenBurnTransaction: Transaction {
+    /// Create a new `TokenBurnTransaction`.
+    public init(
+        tokenId: TokenId? = nil,
+        amount: UInt64 = 0,
+        serialNumbers: [UInt64] = []
+    ) {
+        self.tokenId = tokenId
+        self.amount = amount
+        self.serialNumbers = serialNumbers
+    }
+
+    /// The token for which to burn tokens.
+    public var tokenId: TokenId?
+
+    /// Sets the token for which to burn tokens.
+    @discardableResult
+    public func tokenId(_ tokenId: TokenId) -> Self {
+        self.tokenId = tokenId
+
+        return self
+    }
+
+    /// The amount of a fungible token to burn from the treasury account.
+    public var amount: UInt64
+
+    //// Sets the amount of a fungible token to burn from the treasury account.
+    @discardableResult
+    public func amount(_ amount: UInt64) -> Self {
+        self.amount = amount
+
+        return self
+    }
+
+    /// The serial numbers of a non-fungible token to burn from the treasury account.
+    public var serialNumbers: [UInt64]
+
+    /// Sets the serial numbers of a non-fungible token to burn from the treasury account.
+    @discardableResult
+    public func serialNumbers(_ serialNumbers: [UInt64]) -> Self {
+        self.serialNumbers = serialNumbers
+
+        return self
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case tokenId
+        case amount
+        case serialNumbers
+    }
+
+    public override func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+
+        try container.encode(tokenId, forKey: .tokenId)
+        try container.encode(amount, forKey: .amount)
+        try container.encode(serialNumbers, forKey: .serialNumbers)
+
+        try super.encode(to: encoder)
+    }
+}
---
+++ sdk/swift/Sources/Hedera/TokenUnfreezeTransaction.swift
@@ -0,0 +1,47 @@
+/// Unfreezes transfers of the specified token for the account.
+public class TokenUnfreezeTransaction: Transaction {
+    /// Create a new `TokenUnfreezeTransaction`.
+    public init(
+        accountId: AccountAddress? = nil,
+        tokenId: TokenId? = nil
+    ) {
+        self.accountId = accountId
+        self.tokenId = tokenId
+    }
+
+    /// The account to be unfrozen.
+    public var accountId: AccountAddress?
+
+    /// Sets the account to be unfrozen.
+    @discardableResult
+    public func accountId(_ accountId: AccountAddress?) -> Self {
+        self.accountId = accountId
+
+        return self
+    }
+
+    /// The token for which this account will be unfrozen.
+    public var tokenId: TokenId?
+
+    /// Sets the token for which this account will be unfrozen.
+    @discardableResult
+    public func tokenId(_ tokenId: TokenId) -> Self {
+        self.tokenId = tokenId
+
+        return self
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case accountId
+        case tokenId
+    }
+
+    public override func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+
+        try container.encode(accountId, forKey: .accountId)
+        try container.encode(tokenId, forKey: .tokenId)
+
+        try super.encode(to: encoder)
+    }
+}
