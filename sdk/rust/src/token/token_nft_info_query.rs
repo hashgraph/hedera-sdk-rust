8764359dcbfd59835ff277fd9aa16f90f9efe194
Will Parks <will.parks@launchbadge.com>
2022-06-17 15:29:06 -0700

    feat(rust): implement `TokenNftInfoQuery`
    Signed-off-by: Will Parks <will.parks@launchbadge.com>

---
+++ sdk/rust/src/token/token_nft_info_query.rs
@@ -0,0 +1,60 @@
+use async_trait::async_trait;
+use hedera_proto::services;
+use hedera_proto::services::token_service_client::TokenServiceClient;
+use tonic::transport::Channel;
+
+use crate::query::{AnyQueryData, Query, QueryExecute, ToQueryProtobuf};
+use crate::{TokenNftInfoResponse, NftId, ToProtobuf};
+
+/// Applicable only to tokens of type `NonFungibleUnique`.
+///
+/// Gets info on an NFT for a given TokenID and serial number
+pub type TokenNftInfoQuery = Query<TokenNftInfoQueryData>;
+
+#[derive(Clone, Default, serde::Serialize, serde::Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct TokenNftInfoQueryData {
+    /// The ID of the NFT
+    nft_id: Option<NftId>
+}
+
+impl From<TokenNftInfoQueryData> for AnyQueryData {
+    #[inline]
+    fn from(data: TokenNftInfoQueryData) -> Self {
+        Self::TokenNftInfo(data)
+    }
+}
+
+impl TokenNftInfoQuery {
+    /// Sets the file ID for which contents are requested.
+    pub fn nft_id(&mut self, nft_id: impl Into<NftId>) -> &mut Self {
+        self.data.nft_id = Some(nft_id.into());
+        self
+    }
+}
+
+impl ToQueryProtobuf for TokenNftInfoQueryData {
+    fn to_query_protobuf(&self, header: services::QueryHeader) -> services::Query {
+        let nft_id = self.nft_id.as_ref().map(NftId::to_protobuf);
+
+        services::Query {
+            query: Some(services::query::Query::TokenGetNftInfo(services::TokenGetNftInfoQuery {
+                header: Some(header),
+                nft_id
+            })),
+        }
+    }
+}
+
+#[async_trait]
+impl QueryExecute for TokenNftInfoQueryData {
+    type Response = TokenNftInfoResponse;
+
+    async fn execute(
+        &self,
+        channel: Channel,
+        request: services::Query,
+    ) -> Result<tonic::Response<services::Response>, tonic::Status> {
+        TokenServiceClient::new(channel).get_token_nft_info(request).await
+    }
+}
