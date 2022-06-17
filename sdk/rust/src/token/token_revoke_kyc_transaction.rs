464519fc62ff64057ea30e4d6b326a4bef438aab
Ryan Leckey <ryan@launchbadge.com>
2022-06-16 17:19:20 -0700

    docs(rust): update field and method docs for GrantKyc and RevokeKyc transactions

--- sdk/rust/src/token/token_revoke_kyc_transaction.rs
+++ sdk/rust/src/token/token_revoke_kyc_transaction.rs
@@ -1,14 +1,16 @@
 use async_trait::async_trait;
-use tonic::transport::Channel;
 use hedera_proto::services;
 use hedera_proto::services::token_service_client::TokenServiceClient;
 use serde_with::{serde_as, skip_serializing_none};
+use tonic::transport::Channel;
 
 use crate::protobuf::ToProtobuf;
-use crate::{AccountId, TokenId, Transaction, TransactionId};
 use crate::transaction::{AnyTransactionData, ToTransactionDataProtobuf, TransactionExecute};
+use crate::{AccountId, TokenId, Transaction, TransactionId};
 
-/// Revokes KYC to the account for the given token. Must be signed by the Token's kycKey.
+/// Revokes KYC from the account for the given token.
+///
+/// Must be signed by the Token's kycKey.
 ///
 /// Once executed the Account is marked as KYC Revoked.
 ///
@@ -26,25 +28,21 @@ pub type TokenRevokeKycTransaction = Transaction<TokenRevokeKycTransactionData>;
 #[derive(Debug, Clone, Default, serde::Serialize, serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct TokenRevokeKycTransactionData {
-    /// The account to be KYC Revoked
+    /// The account to have their KYC revoked.
     account_id: Option<AccountId>,
 
-    /// The token for which this account will have KYC revoked.
-    ///
-    /// If token does not exist, transaction results in INVALID_TOKEN_ID
+    /// The token for which this account will have their KYC revoked.
     token_id: Option<TokenId>,
 }
 
 impl TokenRevokeKycTransaction {
-    /// Sets the account to be KYC Revoked
+    /// Sets the account to have their KYC revoked.
     pub fn account_id(&mut self, account_id: impl Into<AccountId>) -> &mut Self {
         self.body.data.account_id = Some(account_id.into());
         self
     }
 
-    /// Sets the token for which this account will have KYC revoked.
-    ///
-    /// If token does not exist, transaction results in INVALID_TOKEN_ID
+    /// Sets the token for which this account will have their KYC revoked.
     pub fn token_id(&mut self, token_id: impl Into<TokenId>) -> &mut Self {
         self.body.data.token_id = Some(token_id.into());
         self
