58fe4a49d3aed253f4ea8029d3997594faa023ce
Ryan Leckey <ryan@launchbadge.com>
2022-06-17 10:22:31 -0700

    feat(rust): add ContractExecute transaction
    Signed-off-by: Ryan Leckey <ryan@launchbadge.com>

--- sdk/rust/src/contract/mod.rs
+++ sdk/rust/src/contract/mod.rs
@@ -1,5 +1,6 @@
 mod contract_create_transaction;
 mod contract_delete_transaction;
+mod contract_execute_transaction;
 mod contract_id;
 mod contract_update_transaction;
 
@@ -7,6 +8,8 @@ pub use contract_create_transaction::ContractCreateTransaction;
 pub(crate) use contract_create_transaction::ContractCreateTransactionData;
 pub use contract_delete_transaction::ContractDeleteTransaction;
 pub(crate) use contract_delete_transaction::ContractDeleteTransactionData;
+pub use contract_execute_transaction::ContractExecuteTransaction;
+pub(crate) use contract_execute_transaction::ContractExecuteTransactionData;
 pub use contract_id::{ContractAddress, ContractEvmAddress, ContractId};
 pub use contract_update_transaction::ContractUpdateTransaction;
 pub(crate) use contract_update_transaction::ContractUpdateTransactionData;
