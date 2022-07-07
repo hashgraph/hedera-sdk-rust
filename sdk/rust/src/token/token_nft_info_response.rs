e12fd8302a281d98a24523a9a01747660bb0f7fa
Daniel Akhterov <daniel@launchbadge.com>
2022-07-07 16:30:27 -0700

    Squashed commit of the following:
    commit 27027e4cb44e5091fa9f7628dfa657524317d263
    Author: Will Parks <will.parks@launchbadge.com>
    Date:   Tue Jun 28 10:54:13 2022 -0700
    
        fix(rust): update `spender_id` mapping to allow conversion errors through
    
        Signed-off-by: Will Parks <will.parks@launchbadge.com>
    
    commit e823ae2f1d0ecd2eaac51fe2df88aaeb7e6d793c
    Author: Will Parks <will.parks@launchbadge.com>
    Date:   Wed Jun 22 17:41:06 2022 -0700
    
        fix(rust): add `TokenGetNftInfo` to `AnyQueryResponse::from_protobuf()`
    
        Signed-off-by: Will Parks <will.parks@launchbadge.com>
    
    commit c18d99777ed849f2cc311a4e95585d5999206f6f
    Author: Will Parks <will.parks@launchbadge.com>
    Date:   Wed Jun 22 17:03:59 2022 -0700
    
        fix(rust): change `account_id` and `spender_id` type to `AccountId` and make `spender_id` optional
    
        Signed-off-by: Will Parks <will.parks@launchbadge.com>
    
    commit afc8ef948b97fe61e6ed0b2d9f2041896fc1dd43
    Author: Will Parks <will.parks@launchbadge.com>
    Date:   Wed Jun 22 16:58:22 2022 -0700
    
        feat(rust): implement `From<(TokenId, i64)>` for `NftId`, and add corresponding tests
    
        Signed-off-by: Will Parks <will.parks@launchbadge.com>
    
    commit 304d836b1ef2a097207fe00c40e3d8c855b8012b
    Author: Will Parks <will.parks@launchbadge.com>
    Date:   Wed Jun 22 16:57:28 2022 -0700
    
        fix(rust): address test compilation errors
    
        Signed-off-by: Will Parks <will.parks@launchbadge.com>
    
    commit 8764359dcbfd59835ff277fd9aa16f90f9efe194
    Author: Will Parks <will.parks@launchbadge.com>
    Date:   Fri Jun 17 15:29:06 2022 -0700
    
        feat(rust): implement `TokenNftInfoQuery`
    
        Signed-off-by: Will Parks <will.parks@launchbadge.com>
    
    Signed-off-by: Daniel Akhterov <daniel@launchbadge.com>

---
+++ sdk/rust/src/token/token_nft_info_response.rs
@@ -0,0 +1,63 @@
+use hedera_proto::services;
+use serde::{Deserialize, Serialize};
+use serde_with::base64::Base64;
+use serde_with::serde_as;
+use time::OffsetDateTime;
+
+use crate::{AccountId, FromProtobuf, NftId};
+
+/// Response from [`TokenNftInfoQuery`][crate::TokenNftInfoQuery].
+#[serde_as]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct TokenNftInfoResponse {
+    /// The ID of the NFT.
+    pub nft_id: NftId,
+
+    /// The current owner of the NFT.
+    pub account_id: AccountId,
+
+    /// Effective consensus timestamp at which the NFT was minted.
+    pub creation_time: OffsetDateTime,
+
+    /// The unique metadata of the NFT
+    #[serde_as(as = "Base64")]
+    pub metadata: Vec<u8>,
+
+    // /// The ledger ID the response was returned from.
+    // TODO pub ledger_id: LedgerId, --- also shows as todo in account_info.rs
+
+    /// If an allowance is granted for the NFT, its corresponding spender account.
+    pub spender_id: Option<AccountId>,
+}
+
+impl FromProtobuf for TokenNftInfoResponse {
+    type Protobuf = services::response::Response;
+
+    fn from_protobuf(pb: Self::Protobuf) -> crate::Result<Self>
+        where
+            Self: Sized,
+    {
+        let pb = pb_getv!(pb, TokenGetNftInfo, services::response::Response);
+        let nft = pb_getf!(pb, nft)?;
+
+        let nft_id = pb_getf!(nft, nft_id)?;
+        let account_id = pb_getf!(nft, account_id)?;
+        let creation_time = nft.creation_time.unwrap();
+        let metadata = nft.metadata;
+        // TODO let ledger_id = nft.ledger_id;
+
+        let spender_id = nft.spender_id
+            .map(AccountId::from_protobuf)
+            .transpose()?;
+
+        Ok(Self {
+            nft_id: NftId::from_protobuf(nft_id)?,
+            account_id: AccountId::from_protobuf(account_id)?,
+            creation_time: OffsetDateTime::from(creation_time),
+            metadata,
+            // TODO ledger_id: Ledger_Id::from_protobuf(ledger_id),
+            spender_id
+        })
+    }
+}
