5a02b8467f3ca68c7f71917c1bb378d6b4289866
Ryan Leckey <ryan@launchbadge.com>
2022-07-17 13:18:39 -0700

    feat(swift): add SystemDelete and SystemUndelete transactions
    Signed-off-by: Ryan Leckey <ryan@launchbadge.com>

---
+++ sdk/swift/Sources/Hedera/SystemDeleteTransaction.swift
@@ -0,0 +1,66 @@
+import Foundation
+
+/// Delete a file or smart contract - can only be done with a Hedera admin.
+public class SystemDeleteTransaction: Transaction {
+    /// Create a new `SystemDeleteTransaction`.
+    public init(
+            fileId: FileId? = nil,
+            contractId: ContractId? = nil,
+            expirationTime: Date? = nil
+    ) {
+        self.fileId = fileId
+        self.contractId = contractId
+        self.expirationTime = expirationTime
+    }
+
+    /// The file ID which should be deleted.
+    public var fileId: FileId?
+
+    /// Sets the file ID which should be deleted.
+    @discardableResult
+    public func fileId(_ fileId: FileId) -> Self {
+        self.fileId = fileId
+
+        return self
+    }
+
+    /// The contract ID which should be deleted.
+    public var contractId: ContractId?
+
+    /// Sets the contract ID which should be deleted.
+    @discardableResult
+    public func contractId(_ contractId: ContractId) -> Self {
+        self.contractId = contractId
+
+        return self
+    }
+
+    /// The timestamp at which the "deleted" file should
+    /// truly be permanently deleted.
+    public var expirationTime: Date?
+
+    /// Sets the timestamp at which the "deleted" file should
+    /// truly be permanently deleted.
+    @discardableResult
+    public func expirationTime(_ expirationTime: Date) -> Self {
+        self.expirationTime = expirationTime
+
+        return self
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case fileId
+        case contractId
+        case expirationTime
+    }
+
+    public override func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+
+        try container.encodeIfPresent(fileId, forKey: .fileId)
+        try container.encodeIfPresent(contractId, forKey: .contractId)
+        try container.encodeIfPresent(expirationTime?.unixTimestampNanos, forKey: .expirationTime)
+
+        try super.encode(to: encoder)
+    }
+}
---
+++ sdk/swift/Sources/Hedera/SystemUndeleteTransaction.swift
@@ -0,0 +1,49 @@
+import Foundation
+
+/// Undelete a file or smart contract that was deleted by SystemDelete.
+public class SystemUndeleteTransaction: Transaction {
+    /// Create a new `SystemUndeleteTransaction`.
+    public init(
+            fileId: FileId? = nil,
+            contractId: ContractId? = nil
+    ) {
+        self.fileId = fileId
+        self.contractId = contractId
+    }
+
+    /// The file ID to undelete.
+    public var fileId: FileId?
+
+    /// Sets the file ID to undelete.
+    @discardableResult
+    public func fileId(_ fileId: FileId) -> Self {
+        self.fileId = fileId
+
+        return self
+    }
+
+    /// The contract ID to undelete.
+    public var contractId: ContractId?
+
+    /// Sets the contract ID to undelete.
+    @discardableResult
+    public func contractId(_ contractId: ContractId) -> Self {
+        self.contractId = contractId
+
+        return self
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case fileId
+        case contractId
+    }
+
+    public override func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+
+        try container.encodeIfPresent(fileId, forKey: .fileId)
+        try container.encodeIfPresent(contractId, forKey: .contractId)
+
+        try super.encode(to: encoder)
+    }
+}
