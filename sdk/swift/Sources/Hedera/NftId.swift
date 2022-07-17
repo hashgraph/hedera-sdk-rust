bc764f0c7935b9f8729301cd0007b88664a0fff5
Ryan Leckey <ryan@launchbadge.com>
2022-07-17 16:35:10 -0700

    feat(swift): extend NftId for string and JSON decoding
    Signed-off-by: Ryan Leckey <ryan@launchbadge.com>

--- sdk/swift/Sources/Hedera/NftId.swift
+++ sdk/swift/Sources/Hedera/NftId.swift
@@ -1,7 +1,7 @@
+import CHedera
+
 /// The unique identifier for a non-fungible token (NFT) instance on Hedera.
-// TODO: convert from string
-// TODO: comparable
-public class NftId: Encodable, CustomStringConvertible {
+public class NftId: Codable, LosslessStringConvertible, ExpressibleByStringLiteral, Equatable {
     /// The (non-fungible) token of which this NFT is an instance.
     public let tokenId: TokenId
 
@@ -14,10 +14,37 @@ public class NftId: Encodable, CustomStringConvertible {
         self.serialNumber = serialNumber
     }
 
+    public required convenience init?(_ description: String) {
+        var shard: UInt64 = 0
+        var realm: UInt64 = 0
+        var num: UInt64 = 0
+        var serialNumber: UInt64 = 0
+
+        let err = hedera_nft_id_from_string(description, &shard, &realm, &num, &serialNumber)
+
+        if err != HEDERA_ERROR_OK {
+            return nil
+        }
+
+        self.init(tokenId: TokenId(shard: shard, realm: realm, num: num), serialNumber: serialNumber)
+    }
+
+    public required convenience init(stringLiteral value: StringLiteralType) {
+        self.init(value)!
+    }
+
+    public required convenience init(from decoder: Decoder) throws {
+        self.init(try decoder.singleValueContainer().decode(String.self))!
+    }
+
     public func encode(to encoder: Encoder) throws {
         var container = encoder.singleValueContainer()
 
         try container.encode(String(describing: self))
+    }
+
+    public static func == (lhs: NftId, rhs: NftId) -> Bool {
+        lhs.serialNumber == rhs.serialNumber && lhs.tokenId == rhs.tokenId
     }
 
     public var description: String {
