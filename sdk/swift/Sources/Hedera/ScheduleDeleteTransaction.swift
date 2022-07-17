94c5ca99a16c5c2d1eb2644dd6a9d9ee5af6bba7
Ryan Leckey <ryan@launchbadge.com>
2022-07-17 13:11:12 -0700

    feat(swift): add ScheduleDelete transaction
    Signed-off-by: Ryan Leckey <ryan@launchbadge.com>

---
+++ sdk/swift/Sources/Hedera/ScheduleDeleteTransaction.swift
@@ -0,0 +1,32 @@
+/// Marks a schedule in the network's action queue as deleted.
+public final class ScheduleDeleteTransaction: Transaction {
+    /// Create a new `ScheduleDeleteTransaction`.
+    public init(
+            scheduleId: ScheduleId? = nil
+    ) {
+        self.scheduleId = scheduleId
+    }
+
+    /// The schedule to delete.
+    public var scheduleId: ScheduleId?
+
+    /// Sets the schedule to delete.
+    @discardableResult
+    public func scheduleId(_ scheduleId: ScheduleId) -> Self {
+        self.scheduleId = scheduleId
+
+        return self
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case scheduleId
+    }
+
+    public override func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+
+        try container.encodeIfPresent(scheduleId, forKey: .scheduleId)
+
+        try super.encode(to: encoder)
+    }
+}
