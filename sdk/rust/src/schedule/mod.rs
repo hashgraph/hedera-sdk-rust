3bce075201322615975e3b67d29437287bfcbac2
Ryan Leckey <ryan@launchbadge.com>
2022-07-17 15:13:01 -0700

    feat(rust): add ScheduleInfo query
    Signed-off-by: Ryan Leckey <ryan@launchbadge.com>

--- sdk/rust/src/schedule/mod.rs
+++ sdk/rust/src/schedule/mod.rs
@@ -1,6 +1,8 @@
 mod schedule_create_transaction;
 mod schedule_delete_transaction;
 mod schedule_id;
+mod schedule_info;
+mod schedule_info_query;
 mod schedule_sign_transaction;
 
 pub use schedule_create_transaction::ScheduleCreateTransaction;
@@ -8,5 +10,8 @@ pub(crate) use schedule_create_transaction::ScheduleCreateTransactionData;
 pub use schedule_delete_transaction::ScheduleDeleteTransaction;
 pub(crate) use schedule_delete_transaction::ScheduleDeleteTransactionData;
 pub use schedule_id::ScheduleId;
+pub use schedule_info::ScheduleInfo;
+pub use schedule_info_query::ScheduleInfoQuery;
+pub(crate) use schedule_info_query::ScheduleInfoQueryData;
 pub use schedule_sign_transaction::ScheduleSignTransaction;
 pub(crate) use schedule_sign_transaction::ScheduleSignTransactionData;
