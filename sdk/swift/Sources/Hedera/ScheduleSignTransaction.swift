9e8b412db2a9fd658e74bf32fb5e6665878419e3
Ryan Leckey <ryan@launchbadge.com>
2022-07-17 13:12:35 -0700

    feat(swift): add ScheduleSign transaction
    Signed-off-by: Ryan Leckey <ryan@launchbadge.com>

---
+++ sdk/swift/Sources/Hedera/ScheduleSignTransaction.swift
@@ -0,0 +1,32 @@
+/// Adds zero or more signing keys to a schedule.
+public final class ScheduleSignTransaction: Transaction {
+    /// Create a new `ScheduleSignTransaction`.
+    public init(
+            scheduleId: ScheduleId? = nil
+    ) {
+        self.scheduleId = scheduleId
+    }
+
+    /// The schedule to add signing keys to.
+    public var scheduleId: ScheduleId?
+
+    /// Set the schedule to add signing keys to.
+    @discardableResult
+    public func scheduleId(_ scheduleId: ScheduleId) -> Self {
+        self.scheduleId = scheduleId
+
+        return self
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case scheduleId
+    }
+
+    public override func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+
+        try container.encodeIfPresent(scheduleId, forKey: .scheduleId)
+
+        try super.encode(to: encoder)
+    }
+}
