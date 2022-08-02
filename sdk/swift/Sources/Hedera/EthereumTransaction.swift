27604f289ada73837e3d450eed4884db7801f862
Ryan Leckey <ryan@launchbadge.com>
2022-08-01 19:53:51 -0700

    feat(swift): add EthereumTransaction
    Signed-off-by: Ryan Leckey <ryan@launchbadge.com>

---
+++ sdk/swift/Sources/Hedera/EthereumTransaction.swift
@@ -0,0 +1,78 @@
+import Foundation
+
+/// Submit an Ethereum transaction.
+public final class EthereumTransaction: Transaction {
+    public init(
+        ethereumData: Data? = nil,
+        callDataFileId: FileId? = nil,
+        maxGasAllowanceHbar: UInt64 = 0
+    ) {
+        self.ethereumData = ethereumData
+        self.callDataFileId = callDataFileId
+        self.maxGasAllowanceHbar = maxGasAllowanceHbar
+    }
+
+    /// The raw Ethereum transaction (RLP encoded type 0, 1, and 2).
+    public var ethereumData: Data?
+
+    /// Sets the raw Ethereum transaction (RLP encoded type 0, 1, and 2).
+    @discardableResult
+    public func ethereumData(_ ethereumData: Data) -> Self {
+        self.ethereumData = ethereumData
+
+        return self
+    }
+
+    /// For large transactions (for example contract create) this should be used to
+    /// set the FileId of an HFS file containing the callData
+    /// of the ethereumData. The data in the ethereumData will be re-written with
+    /// the callData element as a zero length string with the original contents in
+    /// the referenced file at time of execution. The ethereumData will need to be
+    /// "rehydrated" with the callData for signature validation to pass.
+    public var callDataFileId: FileId?
+
+    /// Sets a file ID to find the raw Ethereum transaction (RLP encoded type 0, 1, and 2).
+    ///
+    /// For large transactions (for example contract create) this should be used to
+    /// set the FileId of an HFS file containing the callData
+    /// of the ethereumData. The data in the ethereumData will be re-written with
+    /// the callData element as a zero length string with the original contents in
+    /// the referenced file at time of execution. The ethereumData will need to be
+    /// "rehydrated" with the callData for signature validation to pass.
+    ///
+    @discardableResult
+    public func callDataFileId(_ callDataFileId: FileId) -> Self {
+        self.callDataFileId = callDataFileId
+
+        return self
+    }
+
+    /// The maximum amount that the payer of the hedera transaction
+    /// is willing to pay to complete the transaction.
+    public var maxGasAllowanceHbar: UInt64
+
+    /// Sets the maximum amount that the payer of the hedera transaction
+    /// is willing to pay to complete the transaction.
+    @discardableResult
+    public func maxGasAllowanceHbar(_ maxGasAllowanceHbar: UInt64) -> Self {
+        self.maxGasAllowanceHbar = maxGasAllowanceHbar
+
+        return self
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case ethereumData
+        case callDataFileId
+        case maxGasAllowanceHbar
+    }
+
+    public override func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+
+        try container.encodeIfPresent(ethereumData?.base64EncodedString(), forKey: .ethereumData)
+        try container.encodeIfPresent(callDataFileId, forKey: .callDataFileId)
+        try container.encodeIfPresent(maxGasAllowanceHbar, forKey: .maxGasAllowanceHbar)
+
+        try super.encode(to: encoder)
+    }
+}
