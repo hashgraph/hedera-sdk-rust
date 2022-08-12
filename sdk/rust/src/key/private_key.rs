01f56cf6ba51f6ac12870abf3b0446dac73b8f57
Skyler Ross <skyler@launchbadge.com>
2022-08-12 14:58:03 -0700

    feat(rust): support `derive` for ed25519 private keys
    Signed-off-by: Skyler Ross <skyler@launchbadge.com>

--- sdk/rust/src/key/private_key.rs
+++ sdk/rust/src/key/private_key.rs
@@ -34,14 +34,20 @@ use ed25519_dalek::{
     Keypair,
     Signer,
 };
-use hmac::Hmac;
+use hmac::{
+    Hmac,
+    Mac,
+};
 use k256::pkcs8::der::Encode;
 use pkcs8::der::Decode;
 use pkcs8::{
     AssociatedOid,
     ObjectIdentifier,
 };
-use rand::thread_rng;
+use rand::{
+    thread_rng,
+    Rng,
+};
 use sha2::Sha512;
 
 use crate::{
@@ -54,7 +60,50 @@ pub(super) const ED25519_OID: ObjectIdentifier = ObjectIdentifier::new_unwrap("1
 
 /// A private key on the Hedera network.
 #[derive(Clone)]
-pub struct PrivateKey(Arc<PrivateKeyData>);
+pub struct PrivateKey(Arc<PrivateKeyDataWrapper>);
+
+// find a better name
+struct PrivateKeyDataWrapper {
+    data: PrivateKeyData,
+    chain_code: Option<[u8; 32]>,
+}
+
+impl PrivateKeyDataWrapper {
+    fn new(inner: PrivateKeyData) -> Self {
+        Self { data: inner, chain_code: None }
+    }
+
+    fn new_derivable(inner: PrivateKeyData, chain_code: [u8; 32]) -> Self {
+        Self { data: inner, chain_code: Some(chain_code) }
+    }
+}
+
+// for usage in tests (provides a way to snapshot test)
+impl Debug for PrivateKeyDataWrapper {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        #[derive(Debug)]
+        enum Algorithm {
+            Ed25519,
+            EcdsaSecp256k1,
+        }
+
+        let (algorithm, key) = match &self.data {
+            PrivateKeyData::Ed25519(key) => {
+                (Algorithm::Ed25519, hex::encode(key.secret.as_bytes()))
+            }
+
+            PrivateKeyData::EcdsaSecp256k1(key) => {
+                (Algorithm::EcdsaSecp256k1, hex::encode(&key.to_bytes()))
+            }
+        };
+
+        f.debug_struct("PrivateKeyData")
+            .field("algorithm", &algorithm)
+            .field("key", &key)
+            .field("chain_code", &self.chain_code.as_ref().map(hex::encode))
+            .finish()
+    }
+}
 
 enum PrivateKeyData {
     Ed25519(ed25519_dalek::Keypair),
@@ -65,10 +114,14 @@ impl PrivateKey {
     /// Generates a new Ed25519 private key.
     #[must_use]
     pub fn generate_ed25519() -> Self {
-        let data = ed25519_dalek::Keypair::generate(&mut thread_rng());
+        let mut csprng = thread_rng();
+        let data = ed25519_dalek::Keypair::generate(&mut csprng);
         let data = PrivateKeyData::Ed25519(data);
 
-        Self(Arc::new(data))
+        let mut chain_code = [0u8; 32];
+        csprng.fill(&mut chain_code);
+
+        Self(Arc::new(PrivateKeyDataWrapper::new_derivable(data, chain_code)))
     }
 
     /// Generates a new ECDSA(secp256k1) private key.
@@ -77,13 +130,13 @@ impl PrivateKey {
         let data = k256::ecdsa::SigningKey::random(&mut thread_rng());
         let data = PrivateKeyData::EcdsaSecp256k1(data);
 
-        Self(Arc::new(data))
+        Self(Arc::new(PrivateKeyDataWrapper::new(data)))
     }
 
     /// Gets the public key which corresponds to this private key.
     #[must_use]
     pub fn public_key(&self) -> PublicKey {
-        match &*self.0 {
+        match &self.0.data {
             PrivateKeyData::Ed25519(key) => PublicKey::ed25519(key.public),
             PrivateKeyData::EcdsaSecp256k1(key) => PublicKey::ecdsa_secp256k1(key.verifying_key()),
         }
@@ -108,7 +161,7 @@ impl PrivateKey {
 
         let data = Keypair { public: (&data).into(), secret: data };
 
-        Ok(Self(Arc::new(PrivateKeyData::Ed25519(data))))
+        Ok(Self(Arc::new(PrivateKeyDataWrapper::new(PrivateKeyData::Ed25519(data)))))
     }
 
     /// Parse a ECDSA(secp256k1) `PrivateKey` from a sequence of bytes.
@@ -120,7 +173,7 @@ impl PrivateKey {
             return Self::from_bytes_pkcs8_der(bytes);
         };
 
-        Ok(Self(Arc::new(PrivateKeyData::EcdsaSecp256k1(data))))
+        Ok(Self(Arc::new(PrivateKeyDataWrapper::new(PrivateKeyData::EcdsaSecp256k1(data)))))
     }
 
     fn from_bytes_pkcs8_der(bytes: &[u8]) -> crate::Result<Self> {
@@ -168,7 +221,7 @@ impl PrivateKey {
     }
 
     fn to_bytes_raw(&self) -> [u8; 32] {
-        match &*self.0 {
+        match &self.0.data {
             PrivateKeyData::Ed25519(key) => key.secret.to_bytes(),
             PrivateKeyData::EcdsaSecp256k1(key) => key.to_bytes().into(),
         }
@@ -177,7 +230,7 @@ impl PrivateKey {
     fn algorithm(&self) -> pkcs8::AlgorithmIdentifier<'_> {
         pkcs8::AlgorithmIdentifier {
             parameters: None,
-            oid: match &*self.0 {
+            oid: match &self.0.data {
                 PrivateKeyData::Ed25519(_) => ED25519_OID,
                 PrivateKeyData::EcdsaSecp256k1(_) => k256::Secp256k1::OID,
             },
@@ -187,9 +240,49 @@ impl PrivateKey {
     pub(crate) fn sign(&self, message: &[u8]) -> SignaturePair {
         let public = self.public_key();
 
-        match &*self.0 {
+        match &self.0.data {
             PrivateKeyData::Ed25519(key) => SignaturePair::ed25519(key.sign(message), public),
-            PrivateKeyData::EcdsaSecp256k1(key) => todo!(),
+            PrivateKeyData::EcdsaSecp256k1(_key) => todo!(),
+        }
+    }
+
+    pub fn derive(&self, index: i32) -> crate::Result<Self> {
+        const HARDEND_MASK: u32 = 1 << 31;
+        let index = index as u32;
+
+        let chain_code = match &self.0.chain_code {
+            Some(chain_code) => chain_code,
+            // Key is not derivable
+            None => todo!(),
+        };
+
+        match &self.0.data {
+            PrivateKeyData::Ed25519(key) => {
+                // force hardened.
+                let index = index | HARDEND_MASK;
+
+                let output: [u8; 64] = Hmac::<Sha512>::new_from_slice(chain_code)
+                    .expect("HMAC can take keys of any size")
+                    .chain_update([0])
+                    .chain_update(key.secret.as_bytes())
+                    .chain_update(index.to_be_bytes())
+                    .finalize()
+                    .into_bytes()
+                    .into();
+
+                // todo: use `split_array_ref` when that's stable.
+                let (left, right) = output.split_at(32);
+
+                // this is exactly 32 bytes
+                let chain_code: [u8; 32] = right.try_into().unwrap();
+
+                let data = ed25519_dalek::SecretKey::from_bytes(&left).unwrap();
+                let data = Keypair { public: (&data).into(), secret: data };
+                let data = PrivateKeyData::Ed25519(data);
+
+                Ok(Self(Arc::new(PrivateKeyDataWrapper::new_derivable(data, chain_code))))
+            }
+            PrivateKeyData::EcdsaSecp256k1(_) => todo!(),
         }
     }
 
@@ -198,7 +291,7 @@ impl PrivateKey {
     /// Derive a private key based on the `index`.
     // ⚠️ unaudited cryptography ⚠️
     pub fn legacy_derive(&self, index: i64) -> crate::Result<Self> {
-        match &*self.0 {
+        match &self.0.data {
             PrivateKeyData::Ed25519(key) => {
                 let entropy = key.secret.as_bytes();
                 let mut seed = Vec::with_capacity(entropy.len() + 8);
@@ -206,7 +299,6 @@ impl PrivateKey {
                 seed.extend_from_slice(&*entropy);
 
                 let i1: i32 = match index {
-                    // fixme: this exact case is untested.
                     0xffffffffff => 0xff,
                     0.. => 0,
                     _ => -1,
@@ -255,29 +347,30 @@ impl FromStr for PrivateKey {
 }
 
 // TODO: from_mnemonic
-// TODO: derive (!)
-// TODO: legacy_derive (!) - k256
+// TODO: derive (!) - secp256k1
+// TODO: legacy_derive (!) - secp256k1
 // TODO: sign_message
 // TODO: sign_transaction
 
 #[cfg(test)]
 mod tests {
     use std::str::FromStr;
+    use std::sync::Arc;
 
+    use expect_test::expect;
+    use hex_literal::hex;
     use pkcs8::AssociatedOid;
 
     use super::{
         PrivateKey,
-        ED25519_OID,
+        PrivateKeyDataWrapper,
     };
 
     #[test]
     fn ed25519_from_str() {
         const S: &str = "302e020100300506032b65700422042098aa82d6125b5efa04bf8372be7931d05cd77f5ef3330b97d6ee7c006eaaf312";
         let pk = PrivateKey::from_str(S).unwrap();
-
-        assert_eq!(pk.algorithm().oid, ED25519_OID);
-
+        // ensure round-tripping works.
         assert_eq!(pk.to_string(), S);
     }
 
@@ -306,5 +399,77 @@ mod tests {
         let private_key_neg_1 = private_key.legacy_derive(-1).unwrap();
 
         assert_eq!(private_key_neg_1.to_string(), "302e020100300506032b657004220420caffc03fdb9853e6a91a5b3c57a5c0031d164ce1c464dea88f3114786b5199e5");
+    }
+
+    #[test]
+    fn ed25519_legacy_derive_2() {
+        let private_key = PrivateKey::from_str(
+            "302e020100300506032b65700422042000c2f59212cb3417f0ee0d38e7bd876810d04f2dd2cb5c2d8f26ff406573f2bd",
+        )
+        .unwrap();
+
+        let private_key_mhw = private_key.legacy_derive(0xffffffffff).unwrap();
+
+        assert_eq!(private_key_mhw.to_string(), "302e020100300506032b6570042204206890dc311754ce9d3fc36bdf83301aa1c8f2556e035a6d0d13c2cccdbbab1242")
+    }
+
+    /// This is for testing purposes only.
+    ///
+    /// # Panics
+    /// If `data` and `chain_code` don't make a valid [`PrivateKey`].
+    fn key_with_chain(data: &str, chain_code: [u8; 32]) -> PrivateKey {
+        let key_without_chain = PrivateKey::from_str(data).unwrap();
+
+        let data = match Arc::try_unwrap(key_without_chain.0) {
+            Ok(it) => it.data,
+            Err(_) => unreachable!(),
+        };
+
+        PrivateKey(Arc::new(PrivateKeyDataWrapper::new_derivable(data, chain_code)))
+    }
+
+    // "iosKey"
+    #[test]
+    fn ed25519_derive_1() {
+        // have to create a private key with a chain code, which means... Hacks!
+        // luckily, we're a unit test, so, we can access private fields.
+        let key = key_with_chain(
+            "302e020100300506032b657004220420a6b9548d7e123ad4c8bc6fee58301e9b96360000df9d03785c07b620569e7728",
+            hex!("cde7f535264f1db4e2ded409396f8c72f8075cc43757bd5a205c97699ea40271"),
+        );
+
+        let child_key = key.derive(0).unwrap();
+
+        expect![[r#"
+            PrivateKeyData {
+                algorithm: Ed25519,
+                key: "5f66a51931e8c99089472e0d70516b6272b94dd772b967f8221e1077f966dbda",
+                chain_code: Some(
+                    "0e5c869c1cf9daecd03edb2d49cf2621412578a352578a4bb7ef4eef2942b7c9",
+                ),
+            }
+        "#]]
+        .assert_debug_eq(&*child_key.0);
+    }
+
+    // "androidKey"
+    #[test]
+    fn ed25519_derive_2() {
+        let key = key_with_chain(
+            "302e020100300506032b65700422042097dbce1988ef8caf5cf0fd13a5374969e2be5f50650abd19314db6b32f96f18e",
+            hex!("b7b406314eb2224f172c1907fe39f807e306655e81f2b3bc4766486f42ef1433")
+        );
+        let child_key = key.derive(0).unwrap();
+
+        expect![[r#"
+            PrivateKeyData {
+                algorithm: Ed25519,
+                key: "c284c25b3a1458b59423bc289e83703b125c8eefec4d5aa1b393c2beb9f2bae6",
+                chain_code: Some(
+                    "a7a1c2d115a988e51efc12c23692188a4796b312a4a700d6c703e4de4cf1a7f6",
+                ),
+            }
+        "#]]
+        .assert_debug_eq(&child_key.0);
     }
 }
