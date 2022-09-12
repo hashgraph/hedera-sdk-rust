f9f474e3d3bd88dc16c5bce07b53a4c8a8392fe0
Ryan Leckey <ryan@launchbadge.com>
2022-09-11 23:34:37 -0700

    feat(swift): add Hbar type
    Signed-off-by: Ryan Leckey <ryan@launchbadge.com>

---
+++ sdk/swift/Sources/Hedera/Hbar.swift
@@ -0,0 +1,102 @@
+import Foundation
+
+/// Common units of hbar.
+/// For the most part they follow SI prefix conventions.
+public enum HbarUnit: UInt64 {
+    case tinybar = 1
+    case microbar = 100
+    case millibar = 100_000
+    case hbar = 100_000_000
+    case kilobar = 100_000_000_000
+    case megabar = 100_000_000_000_000
+    case gigabar = 100_000_000_000_000_000
+}
+
+public struct Hbar: LosslessStringConvertible, Codable, ExpressibleByIntegerLiteral, ExpressibleByStringLiteral, ExpressibleByFloatLiteral {
+    /// A constant value of zero hbars.
+    public static let zero: Hbar = 0
+
+    /// A constant value of the maximum number of hbars.
+    public static let max: Hbar = 50000000000
+
+    /// A constant value of the minimum number of hbars.
+    public static let min: Hbar = -50000000000
+
+    public static func fromTinybars(_ amount: Int64) -> Self {
+        Self(tinybars: amount)
+    }
+
+    public init(stringLiteral value: StringLiteralType) {
+        self.init(value)!
+    }
+
+    public init(integerLiteral value: IntegerLiteralType) {
+        self.init(tinybars: Int64(value))
+    }
+
+    public init(floatLiteral value: FloatLiteralType) {
+        try! self.init(Decimal(value))
+    }
+
+    public init?(_ description: String) {
+        let amount = NSDecimalNumber(string: description).decimalValue
+
+        if (amount.isNaN) {
+            return nil
+        }
+
+        let hbar = try? Self(amount)
+
+        if (hbar == nil) {
+            return nil
+        }
+
+        self = hbar!
+    }
+
+    public init(from decoder: Decoder) throws {
+        self.init(tinybars: try decoder.singleValueContainer().decode(Int64.self))
+    }
+
+    private init(tinybars: Int64) {
+        self.tinybars = tinybars
+    }
+
+    /// Create a new Hbar of the specified, possibly fractional value.
+    public init(_ amount: Decimal) throws {
+        self = try Self(amount, HbarUnit.hbar)
+    }
+
+    /// Create a new Hbar of the specified, possibly fractional value.
+    public init(_ amount: Decimal, _ unit: HbarUnit) throws {
+        let tinybars = amount * Decimal(unit.rawValue);
+
+        if (!(tinybars.isZero || (tinybars.isNormal && tinybars.exponent >= 0))) {
+            throw NSError(domain: "Amount and Unit combination results in a fractional value for tinybar.  Ensure tinybar value is a whole number.", code: 0)
+        }
+
+        self.tinybars = NSDecimalNumber(decimal: tinybars).int64Value
+    }
+
+    private let tinybars: Int64
+
+    /// Convert this hbar value to a different unit.
+    public func to(_ unit: HbarUnit) -> Decimal {
+        Decimal(tinybars) / Decimal(unit.rawValue)
+    }
+
+    /// Convert this hbar value to Tinybars.
+    public func toTinybars() -> Int64 {
+        tinybars
+    }
+
+    public var description: String {
+        to(.hbar).description
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+
+        try container.encode(toTinybars())
+    }
+}
