5c37d906b49202b5a628210367fe157ee67f8e33
Skyler Ross <skyler@launchbadge.com>
2022-09-30 17:05:46 -0700

    chore(pk): allow `to_account_id` doctest to run
    it was not, in fact, broken.
    
    Signed-off-by: Skyler Ross <skyler@launchbadge.com>

--- sdk/rust/src/key/public_key/mod.rs
+++ sdk/rust/src/key/public_key/mod.rs
@@ -295,14 +295,13 @@ impl PublicKey {
     ///
     /// # Examples
     ///
-    /// FIXME: this is 100% broken (but it's not this function's fault).
-    /// ```,no_run
+    /// ```
     /// use hedera::PublicKey;
     ///
     /// let key: PublicKey = "302a300506032b6570032100e0c8ec2758a5879ffac226a13c0c516b799e72e35141a0dd828f94d37988a4b7".parse().unwrap();
     ///
     /// let account_id = key.to_account_id(0, 0);
-    /// assert_eq!(account_id.to_string(), "0.0.<todo>");
+    /// assert_eq!(account_id.to_string(), "0.0.302a300506032b6570032100e0c8ec2758a5879ffac226a13c0c516b799e72e35141a0dd828f94d37988a4b7");
     /// ```
     #[must_use]
     pub fn to_account_id(&self, shard: u64, realm: u64) -> AccountId {
