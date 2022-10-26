2f71ec9497924532ed36728687e002f4e029e2b9
Skyler Ross <skyler@launchbadge.com>
2022-10-25 17:45:48 -0700

    feat(swift): TokenAssociation to/from bytes
    Signed-off-by: Skyler Ross <skyler@launchbadge.com>

--- sdk/c/include/hedera.h
+++ sdk/c/include/hedera.h
@@ -1174,6 +1174,18 @@ enum HederaError hedera_subscribe(const struct HederaClient *client,
                                   const void *context,
                                   void (*callback)(const void *context, enum HederaError err, const char *message));
 
+/**
+ * # Safety
+ * - `bytes` must be valid for reads of up to `bytes_size` bytes.
+ * - `s` must only be freed with `hedera_string_free`,
+ *   notably this means it must not be freed with `free`.
+ */
+enum HederaError hedera_token_association_from_bytes(const uint8_t *bytes,
+                                                     size_t bytes_size,
+                                                     char **s);
+
+enum HederaError hedera_token_association_to_bytes(const char *s, uint8_t **buf, size_t *buf_size);
+
 #ifdef __cplusplus
 } // extern "C"
 #endif // __cplusplus
--- sdk/swift/Sources/Hedera/TokenAssociation.swift
+++ sdk/swift/Sources/Hedera/TokenAssociation.swift
@@ -18,6 +18,9 @@
  * ‍
  */
 
+import CHedera
+ import Foundation
+
 /// A token <-> account association.
 public struct TokenAssociation: Codable {
     /// The token involved in the association.
@@ -25,4 +28,42 @@ public struct TokenAssociation: Codable {
 
     /// The account involved in the association.
     public let accountId: AccountId
+
+        public static func fromBytes(_ bytes: Data) throws -> Self {
+        let json: String = try bytes.withUnsafeBytes { (pointer: UnsafeRawBufferPointer) in
+            var ptr: UnsafeMutablePointer<CChar>? = UnsafeMutablePointer(bitPattern: 0)
+            let err = hedera_token_association_from_bytes(
+                    pointer.baseAddress,
+                    pointer.count,
+                    &ptr
+            )
+
+            if err != HEDERA_ERROR_OK {
+                throw HError(err)!
+            }
+
+            return String(hString: ptr!)
+        }
+
+        return try JSONDecoder().decode(Self.self, from: json.data(using: .utf8)!)
+    }
+
+    private func toBytesInner() throws -> Data {
+        let jsonBytes = try JSONEncoder().encode(self)
+        let json = String(data: jsonBytes, encoding: .utf8)!
+        var buf: UnsafeMutablePointer<UInt8>?
+        var buf_size: Int = 0
+        let err = hedera_token_association_to_bytes(json, &buf, &buf_size)
+
+        if err != HEDERA_ERROR_OK {
+            throw HError(err)!
+        }
+
+        return Data(bytesNoCopy: buf!, count: buf_size, deallocator: Data.unsafeCHederaBytesFree)
+    }
+
+    public func toBytes() -> Data {
+        // can't have `throws` because that's the wrong function signature.
+        try! toBytesInner()
+    }
 }
