01adc2ed357443a0e1a5372752583fae43f32dfe
Skyler Ross <skyler@launchbadge.com>
2022-10-25 17:26:32 -0700

    feat(swift): FileInfo to/from bytes
    Signed-off-by: Skyler Ross <skyler@launchbadge.com>

--- sdk/swift/Sources/Hedera/FileInfo.swift
+++ sdk/swift/Sources/Hedera/FileInfo.swift
@@ -18,6 +18,7 @@
  * ‍
  */
 
+import CHedera
 import Foundation
 
 // TODO: keys
@@ -40,4 +41,42 @@ public final class FileInfo: Codable {
     public let fileMemo: String
 
     public let ledgerId: LedgerId
+
+    public static func fromBytes(_ bytes: Data) throws -> Self {
+        let json: String = try bytes.withUnsafeBytes { (pointer: UnsafeRawBufferPointer) in
+            var ptr: UnsafeMutablePointer<CChar>? = UnsafeMutablePointer(bitPattern: 0)
+            let err = hedera_file_info_from_bytes(
+                    pointer.baseAddress,
+                    pointer.count,
+                    &ptr
+            )
+
+            if err != HEDERA_ERROR_OK {
+                throw HError(err)!
+            }
+
+            return String(hString: ptr!)
+        }
+
+        return try JSONDecoder().decode(Self.self, from: json.data(using: .utf8)!)
+    }
+
+    private func toBytesInner() throws -> Data {
+        let jsonBytes = try JSONEncoder().encode(self)
+        let json = String(data: jsonBytes, encoding: .utf8)!
+        var buf: UnsafeMutablePointer<UInt8>?
+        var buf_size: Int = 0
+        let err = hedera_file_info_to_bytes(json, &buf, &buf_size)
+
+        if err != HEDERA_ERROR_OK {
+            throw HError(err)!
+        }
+
+        return Data(bytesNoCopy: buf!, count: buf_size, deallocator: Data.unsafeCHederaBytesFree)
+    }
+
+    public func toBytes() -> Data {
+        // can't have `throws` because that's the wrong function signature.
+        try! toBytesInner()
+    }
 }
