0ef2017aa7740e5a81f6b93d2d39a57d7314e38b
Skyler Ross <skyler@launchbadge.com>
2022-11-29 14:26:57 -0800

    feat(swift): `TokenNftInfo` fields/methods
    - Add: `TokenNftInfo.toBytes()`
    - Add: `TokenNftInfo.fromBytes()`
    - Add: `TokenNftInfo.ledgerId`
    - Change: `TokenNftInfo.spenderAccountId` -> `spenderId`
    
    Signed-off-by: Skyler Ross <skyler@launchbadge.com>

--- sdk/c/include/hedera.h
+++ sdk/c/include/hedera.h
@@ -1370,6 +1370,18 @@ enum HederaError hedera_token_info_to_bytes(const char *s, uint8_t **buf, size_t
  * - `s` must only be freed with `hedera_string_free`,
  *   notably this means it must not be freed with `free`.
  */
+enum HederaError hedera_token_nft_info_from_bytes(const uint8_t *bytes,
+                                                  size_t bytes_size,
+                                                  char **s);
+
+enum HederaError hedera_token_nft_info_to_bytes(const char *s, uint8_t **buf, size_t *buf_size);
+
+/**
+ * # Safety
+ * - `bytes` must be valid for reads of up to `bytes_size` bytes.
+ * - `s` must only be freed with `hedera_string_free`,
+ *   notably this means it must not be freed with `free`.
+ */
 enum HederaError hedera_topic_info_from_bytes(const uint8_t *bytes, size_t bytes_size, char **s);
 
 enum HederaError hedera_topic_info_to_bytes(const char *s, uint8_t **buf, size_t *buf_size);
--- sdk/rust/src/ffi/mod.rs
+++ sdk/rust/src/ffi/mod.rs
@@ -45,6 +45,7 @@ mod subscribe;
 mod timestamp;
 mod token_association;
 mod token_info;
+mod token_nft_info;
 mod topic_info;
 mod transaction_id;
 mod transaction_receipt;
