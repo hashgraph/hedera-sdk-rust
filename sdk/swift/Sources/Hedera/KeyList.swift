f6e0c42ea601ad49a9e2e69a1117e6ca88342742
Skyler Ross <skyler@launchbadge.com>
2022-12-02 15:24:31 -0800

    feat(internal,swift): `Timestamp` to/from protobuf
    Signed-off-by: Skyler Ross <skyler@launchbadge.com>

--- sdk/swift/Sources/Hedera/KeyList.swift
+++ sdk/swift/Sources/Hedera/KeyList.swift
@@ -1,3 +1,5 @@
+import HederaProtobufs
+
 public struct KeyList: ExpressibleByArrayLiteral, Equatable {
     public typealias ArrayLiteralElement = Key
 
@@ -50,5 +52,43 @@ extension KeyList: Codable {
             keys: try container.decodeIfPresent(.keys) ?? [],
             threshold: try container.decodeIfPresent(.threshold)
         )
+    }
+}
+
+extension KeyList: TryProtobufCodable {
+    internal typealias Protobuf = Proto_KeyList
+
+    internal init(protobuf proto: Protobuf) throws {
+        self.init(keys: try .fromProtobuf(proto.keys))
+    }
+
+    internal func toProtobuf() -> Protobuf {
+        .with { $0.keys = keys.toProtobuf() }
+    }
+}
+
+extension KeyList {
+    internal init(protobuf proto: Proto_ThresholdKey) throws {
+        self.init(
+            keys: try .fromProtobuf(proto.keys.keys),
+            threshold: Int(proto.threshold)
+        )
+    }
+
+    internal static func fromProtobuf(_ proto: Proto_ThresholdKey) throws -> Self {
+        try Self(protobuf: proto)
+    }
+
+    internal func toProtobufKey() -> Proto_Key.OneOf_Key {
+        if let threshold = threshold {
+            return .thresholdKey(
+                .with { proto in
+                    proto.keys = toProtobuf()
+                    proto.threshold = UInt32(threshold)
+                }
+            )
+        }
+
+        return .keyList(toProtobuf())
     }
 }
