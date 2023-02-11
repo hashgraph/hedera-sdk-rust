b0691a4e247fc60bdf11e41ea07bdb197de69518
Skyler Ross <skyler@launchbadge.com>
2023-02-10 17:25:27 -0800

    feat(rust): `PublicKey::sign_transaction`, `PrivateKey::verify_transaction`
    Signed-off-by: Skyler Ross <skyler@launchbadge.com>

--- sdk/rust/src/transaction/source.rs
+++ sdk/rust/src/transaction/source.rs
@@ -266,7 +266,7 @@ impl TransactionSources {
         }
     }
 
-    pub(super) fn transactions(&self) -> &[services::Transaction] {
+    pub(crate) fn transactions(&self) -> &[services::Transaction] {
         self.transactions.get_or_init(|| {
             self.signed_transactions
                 .iter()
@@ -278,7 +278,7 @@ impl TransactionSources {
         })
     }
 
-    pub(super) fn signed_transactions(&self) -> &[services::SignedTransaction] {
+    pub(crate) fn signed_transactions(&self) -> &[services::SignedTransaction] {
         &self.signed_transactions
     }
 
