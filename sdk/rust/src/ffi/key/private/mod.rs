6a566bc68f47880e8ec9bf60cf497a268892204d
Skyler Ross <skyler@launchbadge.com>
2023-03-14 14:23:07 -0700

    refactor(swift): Do the string part of keys ourself
    IE, to string/from string
    
    Signed-off-by: Skyler Ross <skyler@launchbadge.com>

--- sdk/rust/src/ffi/key/private/mod.rs
+++ sdk/rust/src/ffi/key/private/mod.rs
@@ -18,12 +18,8 @@
  * ‍
  */
 
-use std::ffi::{
-    CStr,
-    CString,
-};
+use std::ffi::CStr;
 use std::os::raw::c_char;
-use std::str::FromStr;
 use std::{
     ptr,
     slice,
@@ -31,10 +27,7 @@ use std::{
 
 use libc::size_t;
 
-use super::{
-    parse_bytes,
-    parse_str,
-};
+use super::parse_bytes;
 use crate::ffi::error::Error;
 use crate::ffi::key::to_bytes;
 use crate::ffi::util;
@@ -154,94 +147,6 @@ pub unsafe extern "C" fn hedera_private_key_from_bytes_der(
 ) -> Error {
     // safety: invariants are passed through from the caller.
     unsafe { parse_bytes(bytes, bytes_size, key, PrivateKey::from_bytes_der) }
-}
-
-/// Parse a Hedera private key from the passed string.
-///
-/// Optionally strips a `0x` prefix.
-/// See [`hedera_private_key_from_bytes`]
-///
-/// # Safety
-/// - `s` must be a valid string
-/// - `key` must be a valid for writes according to [*Rust* pointer rules].
-///
-/// # Errors
-/// - [`Error::KeyParse`] if `s` cannot be parsed into a `PrivateKey`.
-///
-/// [*Rust* pointer rules]: https://doc.rust-lang.org/std/ptr/index.html#safety
-#[no_mangle]
-pub unsafe extern "C" fn hedera_private_key_from_string(
-    s: *const c_char,
-    key: *mut *mut PrivateKey,
-) -> Error {
-    // safety: invariants are passed through from the caller.
-    unsafe { parse_str(s, key, PrivateKey::from_str) }
-}
-
-/// Parse a `PrivateKey` from a der encoded string.
-///
-/// Optionally strips a `0x` prefix.
-/// See [`hedera_private_key_from_bytes_der`].
-///
-/// # Safety
-/// - `s` must be a valid string
-/// - `key` must be a valid for writes according to [*Rust* pointer rules].
-///
-/// # Errors
-/// - [`Error::KeyParse`] if `s` cannot be parsed into a `PrivateKey`.
-///
-/// [*Rust* pointer rules]: https://doc.rust-lang.org/std/ptr/index.html#safety
-#[no_mangle]
-pub unsafe extern "C" fn hedera_private_key_from_string_der(
-    s: *const c_char,
-    key: *mut *mut PrivateKey,
-) -> Error {
-    // safety: invariants are passed through from the caller.
-    unsafe { parse_str(s, key, PrivateKey::from_str_der) }
-}
-
-/// Parse a Ed25519 `PrivateKey` from a string containing the raw key material.
-///
-/// Optionally strips a `0x` prefix.
-/// See: [`hedera_private_key_from_bytes_ed25519`]
-///
-/// # Safety
-/// - `s` must be a valid string
-/// - `key` must be a valid for writes according to [*Rust* pointer rules].
-///
-/// # Errors
-/// - [`Error::KeyParse`] if `s` cannot be parsed into a ed25519 `PrivateKey`.
-///
-/// [*Rust* pointer rules]: https://doc.rust-lang.org/std/ptr/index.html#safety
-#[no_mangle]
-pub unsafe extern "C" fn hedera_private_key_from_string_ed25519(
-    s: *const c_char,
-    key: *mut *mut PrivateKey,
-) -> Error {
-    // safety: invariants are passed through from the caller.
-    unsafe { parse_str(s, key, PrivateKey::from_str_ed25519) }
-}
-
-/// Parse a ECDSA(secp256k1) `PrivateKey` from a string containing the raw key material.
-///
-/// Optionally strips a `0x` prefix.
-/// See: [`hedera_private_key_from_bytes_ecdsa`]
-///
-/// # Safety
-/// - `s` must be a valid string
-/// - `key` must be a valid for writes according to [*Rust* pointer rules].
-///
-/// # Errors
-/// - [`Error::KeyParse`] if `s` cannot be parsed into a ECDSA(secp256k1) `PrivateKey`.
-///
-/// [*Rust* pointer rules]: https://doc.rust-lang.org/std/ptr/index.html#safety
-#[no_mangle]
-pub unsafe extern "C" fn hedera_private_key_from_string_ecdsa(
-    s: *const c_char,
-    key: *mut *mut PrivateKey,
-) -> Error {
-    // safety: invariants are passed through from the caller.
-    unsafe { parse_str(s, key, PrivateKey::from_str_ecdsa) }
 }
 
 /// Parse a Hedera private key from the passed pem encoded string
@@ -376,68 +281,6 @@ pub unsafe extern "C" fn hedera_private_key_to_bytes_raw(
 ) -> size_t {
     // safety: invariants are passed through from the caller.
     unsafe { to_bytes(key, buf, PrivateKey::to_bytes_raw) }
-}
-
-/// Format a Hedera private key as a string.
-///
-/// Note: the returned string must be freed via `hedera_string_free` in order to prevent a memory leak.
-///
-/// # Safety
-/// - `key` must be a pointer that is valid for reads according to the [*Rust* pointer rules].
-/// - the length of the returned string must not be modified.
-/// - the returned pointer must NOT be freed with `free`.
-///
-/// [*Rust* pointer rules]: https://doc.rust-lang.org/std/ptr/index.html#safety
-#[no_mangle]
-pub unsafe extern "C" fn hedera_private_key_to_string(key: *mut PrivateKey) -> *mut c_char {
-    // potential optimization: `PrivateKey::to_string` just calls `PrivateKey::to_string_der`,
-    // so, this function *could* just call `hedera_private_key_to_string_der`
-    // safety: caller promises that `key` must be valid for reads
-    let key = unsafe { key.as_ref().unwrap() };
-
-    // if this unwrap fails the called method's impl has a bug,
-    // because hex-encoded anything doesn't contain `\0`.
-    CString::new(key.to_string()).unwrap().into_raw()
-}
-
-/// Format a Hedera private key as a der encoded string.
-///
-/// Note: the returned string must be freed via `hedera_string_free` in order to prevent a memory leak.
-///
-/// # Safety
-/// - `key` must be a pointer that is valid for reads according to the [*Rust* pointer rules].
-/// - the length of the returned string must not be modified.
-/// - the returned pointer must NOT be freed with `free`.
-///
-/// [*Rust* pointer rules]: https://doc.rust-lang.org/std/ptr/index.html#safety
-#[no_mangle]
-pub unsafe extern "C" fn hedera_private_key_to_string_der(key: *mut PrivateKey) -> *mut c_char {
-    // safety: caller promises that `key` must be valid for reads
-    let key = unsafe { key.as_ref().unwrap() };
-
-    // if this unwrap fails the called method's impl has a bug,
-    // because hex-encoded anything doesn't contain `\0`.
-    CString::new(key.to_string_der()).unwrap().into_raw()
-}
-
-/// Format a Hedera private key as a string.
-///
-/// Note: the returned string must be freed via `hedera_string_free` in order to prevent a memory leak.
-///
-/// # Safety
-/// - `key` must be a pointer that is valid for reads according to the [*Rust* pointer rules].
-/// - the length of the returned string must not be modified.
-/// - the returned pointer must NOT be freed with `free`.
-///
-/// [*Rust* pointer rules]: https://doc.rust-lang.org/std/ptr/index.html#safety
-#[no_mangle]
-pub unsafe extern "C" fn hedera_private_key_to_string_raw(key: *mut PrivateKey) -> *mut c_char {
-    // safety: caller promises that `key` must be valid for reads
-    let key = unsafe { key.as_ref().unwrap() };
-
-    // if this unwrap fails the called method's impl has a bug,
-    // because hex-encoded anything doesn't contain `\0`.
-    CString::new(key.to_string_raw()).unwrap().into_raw()
 }
 
 // ffi note: `bool` is defined to have the same layout as c17's `_Bool`.
