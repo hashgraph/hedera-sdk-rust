83a2da3f3a1a598e5374d6dbd85bdfb5783b491f
Skyler Ross <skyler@launchbadge.com>
2023-05-15 17:24:57 -0700

    feat(Client): Add a couple getters
    Signed-off-by: Skyler Ross <skyler@launchbadge.com>

--- src/client/operator.rs
+++ src/client/operator.rs
@@ -7,15 +7,17 @@ use crate::{
 
 #[derive(Debug)]
 pub(crate) struct Operator {
-    pub account_id: AccountId,
-    pub signer: AnySigner,
+    pub(crate) account_id: AccountId,
+    pub(crate) signer: AnySigner,
 }
 
 impl Operator {
+    #[must_use]
     pub(crate) fn sign(&self, body_bytes: &[u8]) -> (PublicKey, Vec<u8>) {
         self.signer.sign(body_bytes)
     }
 
+    #[must_use]
     pub(crate) fn generate_transaction_id(&self) -> TransactionId {
         TransactionId::generate(self.account_id)
     }
