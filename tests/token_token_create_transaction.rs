5fafc09c6646e360c9942082f8a27d4a4bb92916
Oliver Kern <oliver.kern@hashgraph-association.com>
2023-05-31 16:31:28 +0200

    feat(tokenCreate): implement keylist

--- tests/token_token_create_transaction.rs
+++ tests/token_token_create_transaction.rs
@@ -13,20 +13,29 @@ use hedera::{
     Key,
     KeyList,
     PrivateKey,
+    Hbar,
+    CustomFee,
+    FixedFee,
+    FractionalFee,
 };
 use crate::common::TestEnvironment;
 use time::Duration;
 
-struct UsedData<KeyType> {
+enum PublicKeyType {
+    PublicKey(PublicKey),
+    KeyList(KeyList),
+}
+struct UsedData {
     name: String,
     token_type: TokenType,
     symbol: String,
     decimals: u32,
     initial_supply: u64,
     account_id: AccountId,
-    public_key: KeyType,
-    custom_fees: u8,
-    max_supply: u64,
+    public_key: PublicKeyType,
+    private_keys: Option<Vec<PrivateKey>>,
+    custom_fees: Vec<(CustomFee<FixedFee>, AccountId)>,
+    max_supply: Option<u64>,
     token_supply_type: TokenSupplyType,
     default_freeze_status: bool,
     auto_renew_period: Duration,
@@ -34,7 +43,7 @@ struct UsedData<KeyType> {
 }
 
 #[tokio::test]
-async fn create_token() {
+async fn test_create_fugible_token() {
     let TestEnvironment { config, client } = setup_global();
 
     let Some(operator) = &config.operator else {
@@ -47,56 +56,66 @@ async fn create_token() {
         panic!("skipping non-free test");
     }
 
-    // Testcase 1
-    let used_data_1: UsedData<PublicKey> = UsedData {
+    let used_data: UsedData = UsedData {
         name: String::from("sdk::rust::e2e::TokenCreateTransaction::1"),
         token_type: TokenType::FungibleCommon,
         symbol: String::from("e2e::ft"),
         decimals: 2,
         initial_supply: 10_000,
         account_id: operator.account_id.clone(),
-        public_key: operator.private_key.clone().public_key(),
-        custom_fees: 20,
-        max_supply: 0,
+        public_key: PublicKeyType::PublicKey(operator.private_key.clone().public_key()),
+        private_keys: None,
+        custom_fees: vec![(FixedFee::new(Hbar::from(1)), operator.account_id.clone())],
+        max_supply: None,
         token_supply_type: TokenSupplyType::Infinite,
         default_freeze_status: false,
         auto_renew_period: Duration::days(80),
         memo: String::from("sdk::rust::e2e::TokenCreateTransaction::1"),
     };
     
-    let token_id_1 = create_new_token(&client, &used_data_1).await;
-    let token_info_1 = get_token_info(&client, token_id_1).await;
+    let token_id = create_new_token(&client, &used_data).await;
+    let token_info = get_token_info(&client, token_id).await;
 
-    check_token_info(token_info_1, &used_data_1);
+    check_token_info(token_info, &used_data);
+}
 
-    // Testcase 2
-    let keylist: Vec<Key> = vec![];
-    for _ in 1..3 {
-        let private = PrivateKey::generate_ed25519();
-        let public = Key::from(private.public_key());
-        keylist.push(public);
+#[tokio::test]
+async fn test_create_non_fungible_token() {
+    let TestEnvironment { config, client } = setup_global();
+
+    let Some(operator) = &config.operator else {
+        log::debug!("skipping test due to lack of operator");
+        panic!("skipping test due to lack of operator");
+    };
+
+    if !config.run_nonfree_tests {
+        log::debug!("skipping non-free test");
+        panic!("skipping non-free test");
     }
 
-    let used_data_2: UsedData<KeyList> = UsedData {
+    let (private_keys, public_keys) = generate_key_list();
+
+    let used_data: UsedData = UsedData {
         name: String::from("sdk::rust::e2e::TokenCreateTransaction::2"),
         token_type: TokenType::NonFungibleUnique,
         symbol: String::from("e2e::nft"),
         decimals: 0,
         initial_supply: 0,
         account_id: operator.account_id.clone(),
-        public_key: KeyList::new().append(&mut keylist),
-        custom_fees: 20,
-        max_supply: 100,
+        public_key: PublicKeyType::KeyList(public_keys),
+        private_keys: Some(private_keys),
+        custom_fees: vec![(FractionalFee::new(Hbar::from(1)), operator.account_id.clone())],
+        max_supply: Some(100),
         token_supply_type: TokenSupplyType::Finite,
         default_freeze_status: true,
         auto_renew_period: Duration::days(90),
         memo: String::from("sdk::rust::e2e::TokenCreateTransaction::2"),
     };
     
-    let token_id_2 = create_new_token(&client, &used_data_2).await;
-    let token_info_2 = get_token_info(&client, token_id_2).await;
+    let token_id = create_new_token(&client, &used_data).await;
+    let token_info = get_token_info(&client, token_id).await;
 
-    check_token_info(token_info_2, &used_data_2);
+    check_token_info(token_info, &used_data);
 
 }
 
@@ -111,26 +130,59 @@ async fn create_new_token(client: &Client, used_data: &UsedData) -> TokenId {
         .decimals(used_data.decimals)
         .initial_supply(used_data.initial_supply)
         .treasury_account_id(used_data.account_id)
-        .admin_key(used_data.public_key.clone())
-        .kyc_key(used_data.public_key.clone())
-        .freeze_key(used_data.public_key.clone())
-        .wipe_key(used_data.public_key.clone())
-        .supply_key(used_data.public_key.clone())
-        .fee_schedule_key(used_data.public_key.clone())
-        .pause_key(used_data.public_key.clone())
-        //TODO: Implement custom fees
-        //.custom_fees(used_data.custom_fees)
+        .custom_fees(used_data.custom_fees)
         .freeze_default(used_data.default_freeze_status)
         .auto_renew_account_id(used_data.account_id)
         .auto_renew_period(used_data.auto_renew_period)
         .token_memo(&used_data.memo)
         .token_supply_type(used_data.token_supply_type);
 
-    if used_data.token_supply_type == TokenSupplyType::Finite {
-        token_create_tx.max_supply(used_data.max_supply);
-    };
+    match &used_data.public_key {
+        PublicKeyType::PublicKey(key) => {
+            token_create_tx
+                .admin_key(key.clone())
+                .kyc_key(key.clone())
+                .freeze_key(key.clone())
+                .wipe_key(key.clone())
+                .supply_key(key.clone())
+                .fee_schedule_key(key.clone())
+                .pause_key(key.clone());
+            ();
+        },
+        PublicKeyType::KeyList(keylist) => {
+            token_create_tx
+                .admin_key(keylist.clone())
+                .kyc_key(keylist.clone())
+                .freeze_key(keylist.clone())
+                .wipe_key(keylist.clone())
+                .supply_key(keylist.clone())
+                .fee_schedule_key(keylist.clone())
+                .pause_key(keylist.clone());
+
+            match &used_data.private_keys {
+                    Some(private_keys) => {
+                        for private_key in private_keys {
+                            token_create_tx.sign(private_key.clone());
+                        }
+                        ();
+                    }
+                    None => panic!("Keylist with public keys but no list with private keys to sign the transaction provided.)"),
+                }
+            ();
+        }
+    }
     
-    let token_create_response = match token_create_tx.execute(client).await {
+    if used_data.token_supply_type == TokenSupplyType::Finite {
+        match used_data.max_supply {
+            Some(max_supply) => {
+                token_create_tx.max_supply(max_supply);
+                ();
+            }
+            None => panic!("Token Supply Type is finite but no max supply was provided.)"),
+        }
+    }
+
+    let token_create_response = match token_create_tx.max_transaction_fee(Hbar::new(100)).execute(client).await {
         Ok(tx_response) => tx_response,
         Err(e) => panic!("Token Create Transaction failed with error: {}", e)
     };
@@ -163,20 +215,60 @@ fn check_token_info(token_info: TokenInfo, used_data: &UsedData) {
     assert_eq!(token_info.decimals, used_data.decimals, "On chain decimals do not match decimals from used_Data.");
     assert_eq!(token_info.total_supply, used_data.initial_supply, "On chain total_supply does not match initial_supply from used_Data.");
     assert_eq!(token_info.treasury_account_id, used_data.account_id, "On chain treasury_account_id does not match account_id from used_Data.");
-    assert_eq!(token_info.admin_key.unwrap(), Key::from(used_data.public_key), "On chain admin_key does not match public_key from used_Data.");
-    assert_eq!(token_info.kyc_key.unwrap(), Key::from(used_data.public_key), "On chain kyc_key does not match public_key from used_Data.");
-    assert_eq!(token_info.freeze_key.unwrap(), Key::from(used_data.public_key), "On chain freeze_key does not match public_key from used_Data.");
-    assert_eq!(token_info.wipe_key.unwrap(), Key::from(used_data.public_key), "On chain wipe_key does not match public_key from used_Data.");
-    assert_eq!(token_info.supply_key.unwrap(), Key::from(used_data.public_key), "On chain supply_key does not match public_key from used_Data.");
-    assert_eq!(token_info.fee_schedule_key.unwrap(), Key::from(used_data.public_key), "On chain fee_schedule_key does not match public_key from used_Data.");
-    assert_eq!(token_info.pause_key.unwrap(), Key::from(used_data.public_key), "On chain pause_key does not match public_key from used_Data.");
-    //assert_eq!(token_info.custom_fees, used_data.custom_fees, "On chain custom_fees do not match custom_fees from used_Data.");
-    assert_eq!(token_info.supply_type, used_data.token_supply_type, "On chain token_supply_type does not match token_supply_type from used_Data.");
-    if used_data.token_supply_type == TokenSupplyType::Finite  {
-        assert_eq!(token_info.max_supply, used_data.max_supply, "On chain max_supply does not match max_supply from used_Data.");
-    }
     assert_eq!(token_info.default_freeze_status.unwrap(), used_data.default_freeze_status, "On chain default_freeze_status does not match default_freeze_status from used_Data.");
     assert_eq!(token_info.auto_renew_account.unwrap(), used_data.account_id, "On chain auto_renew_account does not match account_id from used_Data.");
     assert_eq!(token_info.auto_renew_period.unwrap(), used_data.auto_renew_period, "On chain auto_renew_period does not match auto_renew_period from used_Data.");
     assert_eq!(token_info.token_memo, used_data.memo, "On chain token_memo does not match memo from used_Data.");
+    
+    match &used_data.public_key {
+        PublicKeyType::PublicKey(key) => {
+            let key = Key::from(key.clone());
+            assert_eq!(token_info.admin_key.unwrap(), key, "On chain admin_key does not match public_key from used_Data.");
+            assert_eq!(token_info.kyc_key.unwrap(), key, "On chain kyc_key does not match public_key from used_Data.");
+            assert_eq!(token_info.freeze_key.unwrap(), key, "On chain freeze_key does not match public_key from used_Data.");
+            assert_eq!(token_info.wipe_key.unwrap(), key, "On chain wipe_key does not match public_key from used_Data.");
+            assert_eq!(token_info.supply_key.unwrap(), key, "On chain supply_key does not match public_key from used_Data.");
+            assert_eq!(token_info.fee_schedule_key.unwrap(), key, "On chain fee_schedule_key does not match public_key from used_Data.");
+            assert_eq!(token_info.pause_key.unwrap(), key, "On chain pause_key does not match public_key from used_Data.");
+            ();
+        },
+        PublicKeyType::KeyList(keylist) => {
+            let keylist = Key::from(keylist.clone());
+            assert_eq!(token_info.admin_key.unwrap(), keylist, "On chain admin_key does not match public_key from used_Data.");
+            assert_eq!(token_info.kyc_key.unwrap(), keylist, "On chain kyc_key does not match public_key from used_Data.");
+            assert_eq!(token_info.freeze_key.unwrap(), keylist, "On chain freeze_key does not match public_key from used_Data.");
+            assert_eq!(token_info.wipe_key.unwrap(), keylist, "On chain wipe_key does not match public_key from used_Data.");
+            assert_eq!(token_info.supply_key.unwrap(), keylist, "On chain supply_key does not match public_key from used_Data.");
+            assert_eq!(token_info.fee_schedule_key.unwrap(), keylist, "On chain fee_schedule_key does not match public_key from used_Data.");
+            assert_eq!(token_info.pause_key.unwrap(), keylist, "On chain pause_key does not match public_key from used_Data.");
+            ();
+        }
+    }
+
+    //assert_eq!(token_info.custom_fees, used_data.custom_fees, "On chain custom_fees do not match custom_fees from used_Data.");
+    assert_eq!(token_info.supply_type, used_data.token_supply_type, "On chain token_supply_type does not match token_supply_type from used_Data.");
+    if used_data.token_supply_type == TokenSupplyType::Finite  {
+        match used_data.max_supply {
+            Some(max_supply) => {
+                assert_eq!(token_info.max_supply, max_supply, "On chain max_supply does not match max_supply from used_Data.");
+                ();
+            }
+            None => panic!("Token Supply Type is finite but no max supply was provided.)"),
+        }
+    }
+}
+
+fn generate_key_list() -> (Vec<PrivateKey>, KeyList) {
+    let mut private_keys: Vec<PrivateKey> = vec![];
+    let mut public_keys: Vec<Key> = vec![];
+    for _ in 0..3 {
+        let private = PrivateKey::generate_ed25519();
+        private_keys.push(PrivateKey::from(private.clone()));
+        public_keys.push(Key::from(private.public_key()));
+    }
+    let keylist = KeyList {
+        keys: public_keys,
+        threshold: Some(2),
+    };
+    return (private_keys, keylist)
 }
