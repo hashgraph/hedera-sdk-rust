3e5b227dd24cd28885e7eb51d56e9f97603b589e
Skyler Ross <skyler@launchbadge.com>
2023-06-02 12:46:43 -0700

    test(e2e): add account::allowance_approve
    Signed-off-by: Skyler Ross <skyler@launchbadge.com>

--- tests/e2e/account/mod.rs
+++ tests/e2e/account/mod.rs
@@ -1,8 +1,14 @@
+mod allowance_approve;
 mod create;
 mod delete;
 mod update;
 
-use hedera::AccountBalanceQuery;
+use hedera::{
+    AccountBalanceQuery,
+    AccountId,
+    Hbar,
+    PrivateKey,
+};
 
 use crate::common::{
     setup_global,
@@ -26,4 +32,41 @@ async fn account_balance_query() -> anyhow::Result<()> {
     anyhow::ensure!(balance.account_id == op.account_id);
 
     Ok(())
+}
+
+pub struct Account {
+    pub key: PrivateKey,
+    pub id: AccountId,
+}
+
+impl Account {
+    pub async fn create(initial_balance: Hbar, client: &hedera::Client) -> hedera::Result<Self> {
+        let key = PrivateKey::generate_ed25519();
+
+        let receipt = hedera::AccountCreateTransaction::new()
+            .key(key.public_key())
+            .initial_balance(initial_balance)
+            .execute(client)
+            .await?
+            .get_receipt(client)
+            .await?;
+
+        let account_id = receipt.account_id.unwrap();
+
+        Ok(Self { key, id: account_id })
+    }
+
+    pub async fn delete(self, client: &hedera::Client) -> hedera::Result<()> {
+        hedera::AccountDeleteTransaction::new()
+            .account_id(self.id)
+            .transfer_account_id(client.get_operator_account_id().unwrap())
+            .freeze_with(client)?
+            .sign(self.key)
+            .execute(client)
+            .await?
+            .get_receipt(client)
+            .await?;
+
+        Ok(())
+    }
 }
