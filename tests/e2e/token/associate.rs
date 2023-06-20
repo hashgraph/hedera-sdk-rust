ff0561f4bb79909d136d32487dd91bda55925147
Skyler Ross <skyler@launchbadge.com>
2023-06-20 16:12:37 -0700

    test(e2e): token::associate
    Signed-off-by: Skyler Ross <skyler@launchbadge.com>

---
+++ tests/e2e/token/associate.rs
@@ -0,0 +1,135 @@
+use assert_matches::assert_matches;
+use hedera::{
+    Hbar,
+    Status,
+    TokenAssociateTransaction,
+    TokenCreateTransaction,
+    TokenDeleteTransaction,
+};
+use time::{
+    Duration,
+    OffsetDateTime,
+};
+
+use crate::account::Account;
+use crate::common::{
+    setup_nonfree,
+    TestEnvironment,
+};
+
+#[tokio::test]
+async fn basic() -> anyhow::Result<()> {
+    let Some(TestEnvironment { config: _, client }) = setup_nonfree() else {
+        return Ok(())
+    };
+
+    let (alice, bob) = tokio::try_join!(
+        Account::create(Hbar::new(5), &client),
+        Account::create(Hbar::new(1), &client)
+    )?;
+
+    let token_id = TokenCreateTransaction::new()
+        .name("ffff")
+        .symbol("F")
+        .decimals(3)
+        .initial_supply(0)
+        .treasury_account_id(alice.id)
+        .admin_key(alice.key.public_key())
+        .expiration_time(OffsetDateTime::now_utc() + Duration::minutes(5))
+        .freeze_default(false)
+        .sign(alice.key.clone())
+        .execute(&client)
+        .await?
+        .get_receipt(&client)
+        .await?
+        .token_id
+        .unwrap();
+
+    TokenAssociateTransaction::new()
+        .account_id(bob.id)
+        .token_ids([token_id])
+        .freeze_with(&client)?
+        .sign(bob.key.clone())
+        .execute(&client)
+        .await?
+        .get_receipt(&client)
+        .await?;
+
+    TokenDeleteTransaction::new()
+        .token_id(token_id)
+        .sign(alice.key.clone())
+        .execute(&client)
+        .await?
+        .get_receipt(&client)
+        .await?;
+
+    tokio::try_join!(alice.delete(&client), bob.delete(&client))?;
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn missing_token_id() -> anyhow::Result<()> {
+    let Some(TestEnvironment { config, client }) = setup_nonfree() else {
+        return Ok(())
+    };
+
+    let Some(op) = &config.operator else {
+        log::debug!("skipping test due to lack of operator");
+        return Ok(())
+    };
+
+    TokenAssociateTransaction::new()
+        .account_id(op.account_id)
+        .freeze_with(&client)?
+        .execute(&client)
+        .await?
+        .get_receipt(&client)
+        .await?;
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn missing_account_id_fails() -> anyhow::Result<()> {
+    let Some(TestEnvironment { config: _, client }) = setup_nonfree() else {
+        return Ok(())
+    };
+
+    let res = TokenAssociateTransaction::new().execute(&client).await;
+
+    assert_matches!(
+        res,
+        Err(hedera::Error::TransactionPreCheckStatus {
+            status: Status::InvalidAccountId,
+            transaction_id: _
+        })
+    );
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn missing_signature_fails() -> anyhow::Result<()> {
+    let Some(TestEnvironment { config: _, client }) = setup_nonfree() else {
+        return Ok(())
+    };
+    let account = Account::create(Hbar::new(0), &client).await?;
+
+    let res = TokenAssociateTransaction::new()
+        .account_id(account.id)
+        .freeze_with(&client)?
+        .execute(&client)
+        .await?
+        .get_receipt(&client)
+        .await;
+
+    assert_matches!(
+        res,
+        Err(hedera::Error::ReceiptStatus { status: Status::InvalidSignature, transaction_id: _ })
+    );
+
+    account.delete(&client).await?;
+
+    Ok(())
+}
