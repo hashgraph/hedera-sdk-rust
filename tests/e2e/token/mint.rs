a1aaaf0ba4404c365daadce17d7600a9eaedd5ad
Skyler Ross <skyler@launchbadge.com>
2023-06-23 16:55:56 -0700

    test(e2e): token::mint
    Signed-off-by: Skyler Ross <skyler@launchbadge.com>

---
+++ tests/e2e/token/mint.rs
@@ -0,0 +1,293 @@
+use assert_matches::assert_matches;
+use hedera::{
+    Hbar,
+    Status,
+    TokenBurnTransaction,
+    TokenCreateTransaction,
+    TokenDeleteTransaction,
+    TokenMintTransaction,
+    TokenSupplyType,
+    TokenType,
+};
+use time::{
+    Duration,
+    OffsetDateTime,
+};
+
+use crate::account::Account;
+use crate::common::{
+    setup_nonfree,
+    TestEnvironment,
+};
+use crate::token::FungibleToken;
+
+#[tokio::test]
+async fn basic() -> anyhow::Result<()> {
+    const INITIAL_SUPPLY: u64 = 1_000_000;
+    let Some(TestEnvironment { config: _, client }) = setup_nonfree() else {
+        return Ok(())
+    };
+
+    let account = Account::create(Hbar::new(0), &client).await?;
+    let token = super::FungibleToken::create(&client, &account, INITIAL_SUPPLY).await?;
+
+    let receipt = TokenMintTransaction::new()
+        .amount(10)
+        .token_id(token.id)
+        .sign(account.key.clone())
+        .execute(&client)
+        .await?
+        .get_receipt(&client)
+        .await?;
+
+    assert_eq!(receipt.total_supply, INITIAL_SUPPLY + 10);
+
+    token.burn(&client, INITIAL_SUPPLY + 10).await?;
+    token.delete(&client).await?;
+    account.delete(&client).await?;
+
+    Ok(())
+}
+
+#[tokio::test]
+
+async fn over_supply_limit_fails() -> anyhow::Result<()> {
+    let Some(TestEnvironment { config: _, client }) = setup_nonfree() else {
+        return Ok(())
+    };
+
+    let account = Account::create(Hbar::new(0), &client).await?;
+
+    let token_id = TokenCreateTransaction::new()
+        .name("ffff")
+        .symbol("F")
+        .token_supply_type(hedera::TokenSupplyType::Finite)
+        .max_supply(5)
+        .treasury_account_id(account.id)
+        .admin_key(account.key.public_key())
+        .supply_key(account.key.public_key())
+        .expiration_time(OffsetDateTime::now_utc() + Duration::minutes(5))
+        .sign(account.key.clone())
+        .execute(&client)
+        .await?
+        .get_receipt(&client)
+        .await?
+        .token_id
+        .unwrap();
+
+    let token = FungibleToken { id: token_id, owner: account.clone() };
+
+    let res = TokenMintTransaction::new()
+        .token_id(token.id)
+        .amount(6)
+        .sign(account.key.clone())
+        .execute(&client)
+        .await?
+        .get_receipt(&client)
+        .await;
+
+    assert_matches!(
+        res,
+        Err(hedera::Error::ReceiptStatus {
+            status: Status::TokenMaxSupplyReached,
+            transaction_id: _
+        })
+    );
+
+    token.delete(&client).await?;
+    account.delete(&client).await?;
+
+    Ok(())
+}
+
+#[tokio::test]
+
+async fn missing_token_id_fails() -> anyhow::Result<()> {
+    let Some(TestEnvironment { config: _, client }) = setup_nonfree() else {
+        return Ok(())
+    };
+
+    let res = TokenMintTransaction::new().amount(6).execute(&client).await;
+
+    assert_matches!(
+        res,
+        Err(hedera::Error::TransactionPreCheckStatus {
+            status: Status::InvalidTokenId,
+            transaction_id: _
+        })
+    );
+
+    Ok(())
+}
+
+#[tokio::test]
+
+async fn zero() -> anyhow::Result<()> {
+    const INITIAL_SUPPLY: u64 = 1_000_000;
+    let Some(TestEnvironment { config: _, client }) = setup_nonfree() else {
+        return Ok(())
+    };
+
+    let account = Account::create(Hbar::new(0), &client).await?;
+    let token = super::FungibleToken::create(&client, &account, INITIAL_SUPPLY).await?;
+
+    let receipt = TokenMintTransaction::new()
+        .token_id(token.id)
+        .sign(account.key.clone())
+        .execute(&client)
+        .await?
+        .get_receipt(&client)
+        .await?;
+
+    assert_eq!(receipt.total_supply, INITIAL_SUPPLY);
+
+    token.burn(&client, INITIAL_SUPPLY).await?;
+    token.delete(&client).await?;
+    account.delete(&client).await?;
+
+    Ok(())
+}
+
+#[tokio::test]
+
+async fn missing_supply_key_sig_fails() -> anyhow::Result<()> {
+    let Some(TestEnvironment { config: _, client }) = setup_nonfree() else {
+        return Ok(())
+    };
+
+    let account = Account::create(Hbar::new(0), &client).await?;
+    let token = super::FungibleToken::create(&client, &account, 0).await?;
+
+    let res = TokenMintTransaction::new()
+        .token_id(token.id)
+        .amount(10)
+        .execute(&client)
+        .await?
+        .get_receipt(&client)
+        .await;
+
+    assert_matches!(
+        res,
+        Err(hedera::Error::ReceiptStatus { status: Status::InvalidSignature, transaction_id: _ })
+    );
+
+    token.delete(&client).await?;
+    account.delete(&client).await?;
+
+    Ok(())
+}
+
+#[tokio::test]
+
+async fn nfts() -> anyhow::Result<()> {
+    let Some(TestEnvironment { config: _, client }) = setup_nonfree() else {
+        return Ok(())
+    };
+
+    let account = Account::create(Hbar::new(0), &client).await?;
+
+    let token_id = TokenCreateTransaction::new()
+        .name("ffff")
+        .symbol("F")
+        .treasury_account_id(account.id)
+        .admin_key(account.key.public_key())
+        .supply_key(account.key.public_key())
+        .token_type(TokenType::NonFungibleUnique)
+        .token_supply_type(TokenSupplyType::Finite)
+        .max_supply(5000)
+        .expiration_time(OffsetDateTime::now_utc() + Duration::minutes(5))
+        .sign(account.key.clone())
+        .execute(&client)
+        .await?
+        .get_receipt(&client)
+        .await?
+        .token_id
+        .unwrap();
+
+    let mint_receipt = TokenMintTransaction::new()
+        .token_id(token_id)
+        .metadata((0..10).map(|it| [it]))
+        .sign(account.key.clone())
+        .execute(&client)
+        .await?
+        .get_receipt(&client)
+        .await?;
+
+    assert_eq!(mint_receipt.serials.len(), 10);
+
+    TokenBurnTransaction::new()
+        .serials(mint_receipt.serials)
+        .token_id(token_id)
+        .sign(account.key.clone())
+        .execute(&client)
+        .await?
+        .get_receipt(&client)
+        .await?;
+
+    TokenDeleteTransaction::new()
+        .token_id(token_id)
+        .sign(account.key.clone())
+        .execute(&client)
+        .await?
+        .get_receipt(&client)
+        .await?;
+
+    account.delete(&client).await?;
+
+    Ok(())
+}
+
+#[tokio::test]
+
+async fn nft_metadata_too_long_fails() -> anyhow::Result<()> {
+    let Some(TestEnvironment { config: _, client }) = setup_nonfree() else {
+        return Ok(())
+    };
+
+    let account = Account::create(Hbar::new(0), &client).await?;
+
+    let token_id = TokenCreateTransaction::new()
+        .name("ffff")
+        .symbol("F")
+        .treasury_account_id(account.id)
+        .admin_key(account.key.public_key())
+        .supply_key(account.key.public_key())
+        .token_type(TokenType::NonFungibleUnique)
+        .token_supply_type(TokenSupplyType::Finite)
+        .max_supply(5000)
+        .expiration_time(OffsetDateTime::now_utc() + Duration::minutes(5))
+        .sign(account.key.clone())
+        .execute(&client)
+        .await?
+        .get_receipt(&client)
+        .await?
+        .token_id
+        .unwrap();
+
+    let res = TokenMintTransaction::new()
+        .token_id(token_id)
+        .metadata([[1; 101]])
+        .sign(account.key.clone())
+        .execute(&client)
+        .await;
+
+    assert_matches!(
+        res,
+        Err(hedera::Error::TransactionPreCheckStatus {
+            status: Status::MetadataTooLong,
+            transaction_id: _
+        })
+    );
+
+    TokenDeleteTransaction::new()
+        .token_id(token_id)
+        .sign(account.key.clone())
+        .execute(&client)
+        .await?
+        .get_receipt(&client)
+        .await?;
+
+    account.delete(&client).await?;
+
+    Ok(())
+}
