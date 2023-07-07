2ce33f0f031dc231845fe2dc8ed2c08062be1160
Skyler Ross <skyler@launchbadge.com>
2023-07-07 13:39:38 -0700

    test(e2e): prng transaction
    Signed-off-by: Skyler Ross <skyler@launchbadge.com>

--- tests/e2e/main.rs
+++ tests/e2e/main.rs
@@ -3,6 +3,7 @@ mod common;
 mod contract;
 mod file;
 mod network_version_info;
+mod prng;
 /// Resources for various tests.
 ///
 /// todo: somehow deduplicate this with the version in `examples/`.
