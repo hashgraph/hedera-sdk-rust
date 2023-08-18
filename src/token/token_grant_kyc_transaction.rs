acb6a234e9fbc6b75d74f4cbca280eb7a64e94ab
Skyler Ross <skyler@launchbadge.com>
2023-08-18 16:14:05 -0700

    test(unit): TokenGrantKycTransaction
    Signed-off-by: Skyler Ross <skyler@launchbadge.com>

--- src/token/token_grant_kyc_transaction.rs
+++ src/token/token_grant_kyc_transaction.rs
@@ -157,3 +157,191 @@ impl ToProtobuf for TokenGrantKycTransactionData {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use expect_test::expect;
+    use hedera_proto::services;
+
+    use crate::protobuf::{
+        FromProtobuf,
+        ToProtobuf,
+    };
+    use crate::token::TokenGrantKycTransactionData;
+    use crate::transaction::test_helpers::{
+        transaction_body,
+        unused_private_key,
+        VALID_START,
+    };
+    use crate::{
+        AccountId,
+        AnyTransaction,
+        Hbar,
+        TokenGrantKycTransaction,
+        TokenId,
+        TransactionId,
+    };
+
+    const TEST_TOKEN_ID: TokenId = TokenId::new(4, 2, 0);
+    const TEST_ACCOUNT_ID: AccountId =
+        AccountId { shard: 6, realm: 9, num: 0, alias: None, evm_address: None, checksum: None };
+
+    fn make_transaction() -> TokenGrantKycTransaction {
+        let mut tx = TokenGrantKycTransaction::new();
+
+        tx.node_account_ids(["0.0.5005".parse().unwrap(), "0.0.5006".parse().unwrap()])
+            .transaction_id(TransactionId {
+                account_id: "5006".parse().unwrap(),
+                valid_start: VALID_START,
+                nonce: None,
+                scheduled: false,
+            })
+            .account_id(TEST_ACCOUNT_ID)
+            .token_id(TEST_TOKEN_ID)
+            .max_transaction_fee(Hbar::new(1))
+            .freeze()
+            .unwrap()
+            .sign(unused_private_key());
+
+        tx
+    }
+
+    #[test]
+    fn serialize() {
+        let tx = make_transaction();
+
+        let tx = transaction_body(tx);
+
+        expect![[r#"
+            TransactionBody {
+                transaction_id: Some(
+                    TransactionId {
+                        transaction_valid_start: Some(
+                            Timestamp {
+                                seconds: 1554158542,
+                                nanos: 0,
+                            },
+                        ),
+                        account_id: Some(
+                            AccountId {
+                                shard_num: 0,
+                                realm_num: 0,
+                                account: Some(
+                                    AccountNum(
+                                        5006,
+                                    ),
+                                ),
+                            },
+                        ),
+                        scheduled: false,
+                        nonce: 0,
+                    },
+                ),
+                node_account_id: Some(
+                    AccountId {
+                        shard_num: 0,
+                        realm_num: 0,
+                        account: Some(
+                            AccountNum(
+                                5005,
+                            ),
+                        ),
+                    },
+                ),
+                transaction_fee: 100000000,
+                transaction_valid_duration: Some(
+                    Duration {
+                        seconds: 120,
+                    },
+                ),
+                generate_record: false,
+                memo: "",
+                data: Some(
+                    TokenGrantKyc(
+                        TokenGrantKycTransactionBody {
+                            token: Some(
+                                TokenId {
+                                    shard_num: 4,
+                                    realm_num: 2,
+                                    token_num: 0,
+                                },
+                            ),
+                            account: Some(
+                                AccountId {
+                                    shard_num: 6,
+                                    realm_num: 9,
+                                    account: Some(
+                                        AccountNum(
+                                            0,
+                                        ),
+                                    ),
+                                },
+                            ),
+                        },
+                    ),
+                ),
+            }
+        "#]]
+        .assert_debug_eq(&tx)
+    }
+
+    #[test]
+    fn to_from_bytes() {
+        let tx = make_transaction();
+
+        let tx2 = AnyTransaction::from_bytes(&tx.to_bytes().unwrap()).unwrap();
+
+        let tx = transaction_body(tx);
+
+        let tx2 = transaction_body(tx2);
+
+        assert_eq!(tx, tx2);
+    }
+
+    #[test]
+    fn construct_token_grant_kyc_transaction_from_transaction_body_protobuf() {
+        let tx = services::TokenGrantKycTransactionBody {
+            account: Some(TEST_ACCOUNT_ID.to_protobuf()),
+            token: Some(TEST_TOKEN_ID.to_protobuf()),
+        };
+
+        let data = TokenGrantKycTransactionData::from_protobuf(tx).unwrap();
+
+        assert_eq!(data.account_id, Some(TEST_ACCOUNT_ID));
+        assert_eq!(data.token_id, Some(TEST_TOKEN_ID));
+    }
+
+    #[test]
+    fn get_set_account_id() {
+        let mut tx = TokenGrantKycTransaction::new();
+
+        tx.account_id(TEST_ACCOUNT_ID);
+
+        assert_eq!(tx.get_account_id(), Some(TEST_ACCOUNT_ID));
+    }
+
+    #[test]
+    #[should_panic]
+    fn get_set_account_id_frozen_panic() {
+        let mut tx = make_transaction();
+
+        tx.account_id(TEST_ACCOUNT_ID);
+    }
+
+    #[test]
+    fn get_set_token_id() {
+        let mut tx = TokenGrantKycTransaction::new();
+
+        tx.token_id(TEST_TOKEN_ID);
+
+        assert_eq!(tx.get_token_id(), Some(TEST_TOKEN_ID));
+    }
+
+    #[test]
+    #[should_panic]
+    fn get_set_token_id_frozen_panic() {
+        let mut tx = make_transaction();
+
+        tx.token_id(TEST_TOKEN_ID);
+    }
+}
