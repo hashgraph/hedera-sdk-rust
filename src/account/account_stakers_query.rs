09446b19b47f3cc3f58e7d8cf625ae4a190c6e6c
Skyler Ross <skyler@launchbadge.com>
2023-09-11 12:53:58 -0700

    test(unit): ContractBytecodeQuery
    Signed-off-by: Skyler Ross <skyler@launchbadge.com>

--- src/account/account_stakers_query.rs
+++ src/account/account_stakers_query.rs
@@ -23,9 +23,19 @@ use hedera_proto::services::crypto_service_client::CryptoServiceClient;
 use tonic::transport::Channel;
 
 use crate::ledger_id::RefLedgerId;
-use crate::query::{AnyQueryData, QueryExecute, ToQueryProtobuf};
+use crate::query::{
+    AnyQueryData,
+    QueryExecute,
+    ToQueryProtobuf,
+};
 use crate::{
-    AccountId, AllProxyStakers, BoxGrpcFuture, Error, Query, ToProtobuf, ValidateChecksums,
+    AccountId,
+    AllProxyStakers,
+    BoxGrpcFuture,
+    Error,
+    Query,
+    ToProtobuf,
+    ValidateChecksums,
 };
 
 /// Get all the accounts that are proxy staking to this account.
@@ -95,7 +105,11 @@ mod tests {
     use expect_test::expect;
 
     use crate::query::ToQueryProtobuf;
-    use crate::{AccountId, AccountStakersQuery, Hbar};
+    use crate::{
+        AccountId,
+        AccountStakersQuery,
+        Hbar,
+    };
 
     #[test]
     fn serialize() {
