babbf37adb087db9cbc0ec82048ca3cbb82ef9b3
iamjpotts <8704475+iamjpotts@users.noreply.github.com>
2024-09-30 15:17:47 +0000

    ops: Classify HTTP/2 Go Away and I/O Broken Pipe errors as transient
    Co-authored-by: Ricky Saechao <76449893+RickyLB@users.noreply.github.com>

--- src/execute/error.rs
+++ src/execute/error.rs
@@ -21,8 +21,56 @@ use std::error::Error;
 
 use serde::de::StdError;
 
-/// Punches through all the layers of `tonic::Status` sources to check if this is a `hyper::Error` that is canceled.
-pub(super) fn is_hyper_canceled(status: &tonic::Status) -> bool {
+fn has_transient_io_error<E: StdError>(error: E) -> bool {
+    let Some(source) = error.source() else {
+        return false;
+    };
+
+    if let Some(io_error) = source.downcast_ref::<std::io::Error>() {
+        is_io_error_transient(io_error)
+    } else {
+        false
+    }
+}
+
+// tonic 0.11 (current dependency)
+fn is_hyper_0_error_transient(error: &hyper_0::Error) -> bool {
+    if error.is_canceled() || has_transient_io_error(error) {
+        true
+    } else if let Some(source) = error.source() {
+        if let Some(h2_error) = source.downcast_ref::<h2_03::Error>() {
+            h2_error.is_go_away()
+        } else {
+            false
+        }
+    } else {
+        false
+    }
+}
+
+// tonic 0.12
+fn is_hyper_error_transient(error: &hyper::Error) -> bool {
+    if error.is_canceled() || has_transient_io_error(error) {
+        true
+    } else if let Some(source) = error.source() {
+        if let Some(h2_error) = source.downcast_ref::<h2::Error>() {
+            h2_error.is_go_away()
+        } else {
+            false
+        }
+    } else {
+        false
+    }
+}
+
+fn is_io_error_transient(error: &std::io::Error) -> bool {
+    match error.kind() {
+        std::io::ErrorKind::BrokenPipe => true,
+        _ => false,
+    }
+}
+
+pub(super) fn is_tonic_status_transient(status: &tonic::Status) -> bool {
     let source = status
         .source()
         .and_then(|it| it.downcast_ref::<tonic::transport::Error>())
@@ -33,11 +81,9 @@ pub(super) fn is_hyper_canceled(status: &tonic::Status) -> bool {
     };
 
     if let Some(hyper_0) = source.downcast_ref::<hyper_0::Error>() {
-        // tonic 0.11 (current dependency)
-        hyper_0.is_canceled()
-    } else if let Some(hyper_1) = source.downcast_ref::<hyper::Error>() {
-        // tonic 0.12
-        hyper_1.is_canceled()
+        is_hyper_0_error_transient(hyper_0)
+    } else if let Some(hyper) = source.downcast_ref::<hyper::Error>() {
+        is_hyper_error_transient(hyper)
     } else {
         false
     }
@@ -48,22 +94,22 @@ pub(super) fn is_hyper_canceled(status: &tonic::Status) -> bool {
 /// Because hyper does not expose constructors for its error variants, there is no
 /// reasonable way to construct a test for positive detection of a hyper cancellation.
 #[cfg(test)]
-mod test_is_hyper_canceled {
+mod test_is_tonic_status_transient {
     use tonic::Code;
 
-    use super::is_hyper_canceled;
+    use super::is_tonic_status_transient;
 
     #[test]
     fn ignores_tonic_abort() {
         let input = tonic::Status::new(Code::Aborted, "foo");
 
-        assert!(!is_hyper_canceled(&input));
+        assert!(!is_tonic_status_transient(&input));
     }
 
     #[test]
     fn ignores_tonic_cancel() {
         let input = tonic::Status::new(Code::Cancelled, "foo");
 
-        assert!(!is_hyper_canceled(&input));
+        assert!(!is_tonic_status_transient(&input));
     }
 }
